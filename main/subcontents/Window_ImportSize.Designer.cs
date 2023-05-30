b7236ed4b78fdc455cc0792ca8c5c75bc25db29d
korat1221 <108967572+korat1221@users.noreply.github.com>
2023-05-30 13:52:53 +0900

    importSize 폼

---
+++ main/subcontents/Window_ImportSize.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace main.subcontents
+{
+    partial class Window_ImportSize
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            Size_dataGridView = new DataGridView();
+            Save_button = new Button();
+            CSVImport_button = new Button();
+            ((System.ComponentModel.ISupportInitialize)Size_dataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // Size_dataGridView
+            // 
+            Size_dataGridView.AllowUserToAddRows = false;
+            Size_dataGridView.AllowUserToDeleteRows = false;
+            Size_dataGridView.AllowUserToResizeColumns = false;
+            Size_dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            Size_dataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            Size_dataGridView.BackgroundColor = SystemColors.Control;
+            Size_dataGridView.BorderStyle = BorderStyle.None;
+            Size_dataGridView.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            Size_dataGridView.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle1.Font = new Font("맑은 고딕", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle1.SelectionForeColor = Color.Black;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            Size_dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            Size_dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            Size_dataGridView.Location = new Point(3, 54);
+            Size_dataGridView.Name = "Size_dataGridView";
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            Size_dataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            Size_dataGridView.RowHeadersVisible = false;
+            Size_dataGridView.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.GradientInactiveCaption;
+            dataGridViewCellStyle3.SelectionForeColor = Color.Black;
+            Size_dataGridView.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            Size_dataGridView.RowTemplate.Height = 25;
+            Size_dataGridView.Size = new Size(1144, 359);
+            Size_dataGridView.TabIndex = 19;
+            // 
+            // Save_button
+            // 
+            Save_button.BackColor = SystemColors.ButtonHighlight;
+            Save_button.ForeColor = Color.Black;
+            Save_button.Location = new Point(1000, 422);
+            Save_button.Name = "Save_button";
+            Save_button.Size = new Size(135, 25);
+            Save_button.TabIndex = 20;
+            Save_button.Text = "SAVE";
+            Save_button.UseVisualStyleBackColor = true;
+            Save_button.Click += Save_button_Click;
+            // 
+            // CSVImport_button
+            // 
+            CSVImport_button.BackColor = SystemColors.ControlLight;
+            CSVImport_button.FlatAppearance.BorderColor = SystemColors.ActiveCaption;
+            CSVImport_button.FlatStyle = FlatStyle.System;
+            CSVImport_button.Font = new Font("나눔고딕 ExtraBold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            CSVImport_button.Location = new Point(9, 19);
+            CSVImport_button.Margin = new Padding(0);
+            CSVImport_button.Name = "CSVImport_button";
+            CSVImport_button.Size = new Size(120, 23);
+            CSVImport_button.TabIndex = 90;
+            CSVImport_button.Text = "치수 정보 Import";
+            CSVImport_button.UseVisualStyleBackColor = false;
+            CSVImport_button.Click += CSVImport_button_Click;
+            // 
+            // Window_ImportSize
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1147, 459);
+            Controls.Add(CSVImport_button);
+            Controls.Add(Save_button);
+            Controls.Add(Size_dataGridView);
+            Name = "Window_ImportSize";
+            Text = "Window_ImportSize";
+            ((System.ComponentModel.ISupportInitialize)Size_dataGridView).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private DataGridView Size_dataGridView;
+        private Button Save_button;
+        private Button CSVImport_button;
+    }
+}
---
+++ main/subcontents/Window_ImportSize.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace main.subcontents
+{
+    public partial class Window_ImportSize : Form
+    {
+        public double Count_SizeInfo;
+        ArrayList SelectRow = new ArrayList();
+
+        public Window_ImportSize()
+        {
+            InitializeComponent();
+        }
+
+        private void CSVImport_button_Click(object sender, EventArgs e)
+        {
+            Import_SizeInfo();
+        }
+
+        void Import_SizeInfo()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = ".csv files (*.csv)|*.csv";
+            openFileDialog.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Program.DB.deleteValue(DB.type.CalcDB, "Import_WindowSize");
+
+                try
+                {
+                    using (StreamReader sr = new StreamReader(openFileDialog.FileName))
+                    {
+                        int n = 0;
+                        while (!sr.EndOfStream)
+                        {
+                            string[] token = sr.ReadLine().Split(',');
+                            if (n == 0)
+                            {
+                            }
+                            else
+                            {
+                                Program.DB.setValue(DB.type.CalcDB, "Import_WindowSize", "창호명칭,창호면적,창호너비,창호높이,고정창유리면적,개폐창유리면적,개폐프레임면적,고정프레임면적,중간프레임면적,고정창유리둘레길이,개폐창유리둘레길이",
+                                "'" + token[0] + "','" + token[1] + "','" + token[2] + "','" + token[3] + "','"
+                                + token[4] + "','" + token[5] + "','" + token[6] + "','" + token[7] + "','" + token[8] + "','"
+                                + token[9] + "','" + token[10] + "'", "창호 명칭");
+                            }
+                            n++;
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("파일의 형식이 올바르지않습니다. 데이터를 확인해주세요.");
+                }
+                load_table_SizeInfo();
+            }
+        }
+
+        void load_table_SizeInfo()
+        {
+
+            DataTable table_WindowSize = new DataTable();
+            DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn();
+            Size_dataGridView.Columns.Clear();
+            checkBoxColumn.HeaderText = "선택";
+            checkBoxColumn.Name = "check";
+            Size_dataGridView.Columns.Add(checkBoxColumn);
+            table_WindowSize.Columns.Add("창호명칭", typeof(string));
+            table_WindowSize.Columns.Add("창호면적" + Environment.NewLine + "[m²]", typeof(string));
+            table_WindowSize.Columns.Add("창호너비" + Environment.NewLine + "[m]", typeof(string));
+            table_WindowSize.Columns.Add("창호높이" + Environment.NewLine + "[m]", typeof(string));
+            table_WindowSize.Columns.Add("고정창\r\n유리면적", typeof(string));
+            table_WindowSize.Columns.Add("개폐창\r\n유리면적" + Environment.NewLine + "[m²]", typeof(string));
+            table_WindowSize.Columns.Add("개폐\r\n프레임면적" + Environment.NewLine + "[m²]", typeof(string));
+            table_WindowSize.Columns.Add("고정\r\n프레임면적" + Environment.NewLine + "[m²]", typeof(string));
+            table_WindowSize.Columns.Add("중간\r\n프레임면적" + Environment.NewLine + "[m²]", typeof(string));
+            table_WindowSize.Columns.Add("고정창유리\r\n둘레길이" + Environment.NewLine + "[m]", typeof(string));
+            table_WindowSize.Columns.Add("개폐창유리\r\n둘레길이" + Environment.NewLine + "[m]", typeof(string));
+            string[][] WinSize = Program.DB.getValue(DB.type.CalcDB, "Import_WindowSize", "창호명칭,창호면적,창호너비,창호높이,고정창유리면적,개폐창유리면적,개폐프레임면적,고정프레임면적,중간프레임면적,고정창유리둘레길이,개폐창유리둘레길이");
+
+            for (int n = 0; n < WinSize.Length; n++)
+            {
+                table_WindowSize.Rows.Add(WinSize[n][0], WinSize[n][1], WinSize[n][2], WinSize[n][3], WinSize[n][4], WinSize[n][5], WinSize[n][6], WinSize[n][7], WinSize[n][8], WinSize[n][9], WinSize[n][10]);
+            }
+            Size_dataGridView.DataSource = table_WindowSize;
+            Count_SizeInfo = WinSize.Length;
+        }
+
+        private void SelectCheckBox()
+        {
+            foreach (DataGridViewRow row in Size_dataGridView.Rows)
+            {
+                if (Convert.ToBoolean(row.Cells["check"].Value))
+                {
+                    row.DefaultCellStyle.SelectionBackColor = SystemColors.GradientInactiveCaption;
+                    SelectRow.Add(row.Index);
+                }
+            }
+        }
+
+        private void Save_button_Click(object sender, EventArgs e)
+        {
+            SelectRow.Clear();
+            SelectCheckBox();
+
+            Program.DB.deleteValue(DB.type.CalcDB, "Select_WindowSize");
+            for (int n = 0; n < SelectRow.Count; n++)
+            {
+                DataGridViewRow row = Size_dataGridView.Rows[Convert.ToInt32(SelectRow[n])];
+                Program.DB.setValue(DB.type.CalcDB, "Select_WindowSize", "창호명칭,창호면적,창호너비,창호높이,고정창유리면적,개폐창유리면적,개폐프레임면적,고정프레임면적,중간프레임면적,고정창유리둘레길이,개폐창유리둘레길이",
+                "'" + row.Cells[1].Value.ToString() + "','" + row.Cells[2].Value.ToString() + "','" + row.Cells[3].Value.ToString() + "','" + row.Cells[4].Value.ToString() + "','"
+                + row.Cells[5].Value.ToString() + "','" + row.Cells[6].Value.ToString() + "','" + row.Cells[7].Value.ToString() + "','" + row.Cells[8].Value.ToString() + "','" + row.Cells[9].Value.ToString() + "','"
+                + row.Cells[10].Value.ToString() + "','" + row.Cells[11].Value.ToString() + "'", "창호명칭");
+
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+
+    }
+}
