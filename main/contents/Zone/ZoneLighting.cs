ab8a1303d9ee0b583a546bf12bb41a033ac8e06c
korat1221 <108967572+korat1221@users.noreply.github.com>
2023-06-08 12:09:54 +0900

    조명UI작업중

--- main/contents/Zone/ZoneLighting.cs
+++ main/contents/Zone/ZoneLighting.cs
@@ -19,19 +19,19 @@ namespace main.contents
             InitializeComponent();
 
             //조명방식 콤보박스
-            Program.UTIL.FillComboBox_ByCategory(LightMethod_comboBox, "조명", "조명방식", "1");
+            Program.UTIL.FillComboBox(LightMethod_comboBox, "조명", "조명방식", "1");
 
             //제어방식 콤보박스
-            Program.UTIL.FillComboBox_ByCategory(ControlType_comboBox, "조명", "제어종류", "1");
+            Program.UTIL.FillComboBox(ControlType_comboBox, "조명", "제어종류", "1");
 
             //디밍유형 콤보박스
-            Program.UTIL.FillComboBox_ByCategory(DimmingType_comboBox, "조명", "주광제어종류", "1");
+            Program.UTIL.FillComboBox(DimmingType_comboBox, "조명", "주광제어종류", "1");
 
             //집광채광 향 콤보박스
-            Program.UTIL.FillComboBox_ByCategory(RenewDi_comboBox, "조명", "방위", "1");
+            Program.UTIL.FillComboBox(RenewDi_comboBox, "조명", "방위", "1");
 
             //집광채광 기울기 콤보박스
-            Program.UTIL.FillComboBox_ByCategory(Slope_comboBox, "조명", "창기울기", "1");
+            Program.UTIL.FillComboBox(Slope_comboBox, "조명", "창기울기", "1");
 
 
 
