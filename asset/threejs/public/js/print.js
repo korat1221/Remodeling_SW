507dfa71796720506c49311dea9846dd5d7051d6
may2023wendnesday <133845375+may2023wendnesday@users.noreply.github.com>
2023-07-17 18:07:58 +0900

    결과보고서 작업중

--- asset/threejs/public/js/print.js
+++ asset/threejs/public/js/print.js
@@ -1,36 +1,36 @@
-
 window.addEventListener("message", async (event) => {
-    let o = event.data;
-    if (o.print) {
-        window.print();
+  let o = event.data;
+  if (o.print) {
+    window.print();
+  } else if (o.init) {
+    let i = -1,
+      j;
+
+    $("div").remove();
+
+    while (++i < o.pages.length) {
+      if (i > 0) {
+        $("body").append("<div style='page-break-before:always'></div>");
+      }
+      $("body").append($("<div>").load("/print/" + o.pages[i]));
     }
-    else if (o.init) {
-        let i = -1, j;
 
-        $( "div" ).remove();
+    setTimeout(() => {
+      let i = -1;
 
-        while(++i < o.pages.length) {
-            if (i > 0) {
-                $( 'body' ).append("<div style='page-break-before:always'></div>");
-            }
-            $( 'body' ).append($('<div>').load('/print/' + o.pages[i]));
+      while (++i < o.items.length) {
+        let item = o.items[i];
+
+        j = -1;
+        while (++j < item.data.length) {
+          let el = item.data[j];
+          $("." + item.cname)
+            .eq(el.idx)
+            .html(el.val);
         }
-        
-        setTimeout(() => {
-            let i = -1;
-
-            while(++i < o.items.length) {
-                let item = o.items[i];
-    
-                j = -1;
-                while(++j < item.data.length) {
-                    let el = item.data[j];
-                    $( '.' + item.cname ).eq(el.idx).html(el.val);
-                }
-            }    
-        }, 100);
-    }
+      }
+    }, 100);
+  }
 });
 
-$(function(){
-});
+$(function () {});
