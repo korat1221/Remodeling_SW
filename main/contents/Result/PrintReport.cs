7e4bdc82e1788a529b067aca55cb991f0628c556
may2023wendnesday <133845375+may2023wendnesday@users.noreply.github.com>
2023-07-17 18:09:26 +0900

    결과보고서 작업중

--- main/contents/Result/PrintReport.cs
+++ main/contents/Result/PrintReport.cs
@@ -52,30 +52,44 @@ namespace main.contents.Result
         public void LoadData(String ID)            // 리스트에서 항목 더블 클릭시 - 뷰를 ID 의 getValue 값으로 채우기
         {
             string s, s2;
-            string[][] ZoneG = Program.DB.getValue(DB.type.ProjDB, "Zonegeneral", "zoneName,zoneUsage,zoneHC,θi_h_set,θi_c_set,Δθi_NA,Fx,Fx_fl,Fx_wl,θs_c,θi_h_min,θe_min,θSUP_Wi,Mode_night,Mode_we,twd_d,th_op_d_we,th_op_d,dwd_a,ZoneArea,zoneHeight,qI_p,qI_fac,Cwirk_A,VA_we,VA_wd,n50,e,f,Vmech_SUP_we,Vmech_SUP_wd,Vmech_ETA_we,Vmech_ETA_wd,ηV_mech,ηχV_mech,χi_c_set,χi_h_set,Vmech_SUP_z,Vmech_ETA_z,ρacp_a");
+            string[][] ZoneG = Program.DB.getValue(DB.type.ProjDB, "ZoneGeneral_Form", "존번호,존이름,실제어방식,냉난방유무,환기유무,환기방식,온도교환효율,전열교환효율,용도프로필,천장고,시작시간,종료시간,주이용일,재실자수,기기발열수준,일일급탕요구량,냉난방시간,사용시간,공조시간,연이용일수,재실밀도,재실수준,일일인체발열,면적당인체발열,일일기기발열,면적당기기발열,순체적,환기횟수,이용일환기량,비이용일환기량");
             List<object> items = new List<object>();
             List<object> data = new List<object>();
             List<object> _data = new List<object>();
+            List<object> _data2 = new List<object>();
+            List<object> _data3 = new List<object>();
+            List<object> _data4 = new List<object>();
+
             int i = -1;
 
             while (++i < ZoneG.Length)
             {
-                items.Add("zpage.html"); // 예시 코드: 메인 메뉴 동적 할당
+                items.Add("zprint7.html"); // 예시 코드: 메인 메뉴 동적 할당
 
-                s2 = ZoneG[i][1];
-                s = s2.Substring(0, 2);
-                string[][] rec = Program.DB.getValue(DB.type.BaseDB_HCneed, " 용도프로필", "용도명","항목='" + s + "'");
 
-                _data.Add(new { idx = i, val = rec[0][0]}); // 예시 코드: 메인 메뉴 동적 할당
+                _data.Add(new { idx = i, val = ZoneG[i][8] }); 
+                _data2.Add(new { idx = i, val = ZoneG[i][26] });
+                _data3.Add(new { idx = i, val = ZoneG[i][0] });
+                _data4.Add(new { idx = i, val = ZoneG[i][1] });
             }
 
-            data.Add(new { cname = "cls-profile-name", data = _data }); // 예시 코드: 메인 메뉴 동적 할당
+            data.Add(new { cname = "cls-profile-name", data = _data }); 
+            data.Add(new { cname = "cls-volume", data = _data2 });
+            data.Add(new { cname = "cls-sub-name", data = _data3 });
+            data.Add(new { cname = "cls-zone-name", data = _data4 });
+
+
+
+
 
             s = System.Text.Json.JsonSerializer.Serialize(items.ToArray());
             s2 = System.Text.Json.JsonSerializer.Serialize(data.ToArray());
 
             runScript("init(" + s + "," + s2 + ")");
         }
+
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
