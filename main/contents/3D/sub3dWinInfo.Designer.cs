26d0a630247171d27d930fcca357d99b405f9714
arcookie0 <arcookie0@gmail.com>
2023-08-11 15:34:07 +0900

    3D 인식 카테고리 - 창호 상세정보 수정

--- main/contents/3D/sub3dWinInfo.Designer.cs
+++ main/contents/3D/sub3dWinInfo.Designer.cs
@@ -30,6 +30,20 @@
         {
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            label12 = new Label();
+            textBox11 = new TextBox();
+            label10 = new Label();
+            textBox10 = new TextBox();
+            label8 = new Label();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            textBox6 = new TextBox();
+            label9 = new Label();
+            textBox7 = new TextBox();
+            label11 = new Label();
+            textBox8 = new TextBox();
+            label13 = new Label();
+            textBox9 = new TextBox();
             textBox4 = new TextBox();
             label5 = new Label();
             label6 = new Label();
@@ -43,12 +57,10 @@
             label67 = new Label();
             label68 = new Label();
             textBox23 = new TextBox();
-            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
             tabPage2 = new TabPage();
             webView22 = new Microsoft.Web.WebView2.WinForms.WebView2();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
             tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)webView22).BeginInit();
             SuspendLayout();
@@ -67,6 +79,20 @@
             // tabPage1
             // 
             tabPage1.BackColor = Color.White;
+            tabPage1.Controls.Add(label12);
+            tabPage1.Controls.Add(textBox11);
+            tabPage1.Controls.Add(label10);
+            tabPage1.Controls.Add(textBox10);
+            tabPage1.Controls.Add(label8);
+            tabPage1.Controls.Add(textBox5);
+            tabPage1.Controls.Add(label7);
+            tabPage1.Controls.Add(textBox6);
+            tabPage1.Controls.Add(label9);
+            tabPage1.Controls.Add(textBox7);
+            tabPage1.Controls.Add(label11);
+            tabPage1.Controls.Add(textBox8);
+            tabPage1.Controls.Add(label13);
+            tabPage1.Controls.Add(textBox9);
             tabPage1.Controls.Add(textBox4);
             tabPage1.Controls.Add(label5);
             tabPage1.Controls.Add(label6);
@@ -80,13 +106,157 @@
             tabPage1.Controls.Add(label67);
             tabPage1.Controls.Add(label68);
             tabPage1.Controls.Add(textBox23);
-            tabPage1.Controls.Add(webView21);
             tabPage1.Location = new Point(4, 24);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
             tabPage1.Size = new Size(792, 422);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "음영 정보";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(175, 267);
+            label12.Name = "label12";
+            label12.Size = new Size(71, 15);
+            label12.TabIndex = 42;
+            label12.Text = "상인방 높이";
+            label12.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // textBox11
+            // 
+            textBox11.BackColor = Color.White;
+            textBox11.BorderStyle = BorderStyle.None;
+            textBox11.Location = new Point(265, 266);
+            textBox11.Name = "textBox11";
+            textBox11.ReadOnly = true;
+            textBox11.Size = new Size(100, 16);
+            textBox11.TabIndex = 41;
+            textBox11.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(483, 220);
+            label10.Name = "label10";
+            label10.Size = new Size(59, 15);
+            label10.TabIndex = 40;
+            label10.Text = "창호 너비";
+            label10.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // textBox10
+            // 
+            textBox10.BackColor = Color.White;
+            textBox10.BorderStyle = BorderStyle.None;
+            textBox10.Location = new Point(561, 220);
+            textBox10.Name = "textBox10";
+            textBox10.ReadOnly = true;
+            textBox10.Size = new Size(100, 16);
+            textBox10.TabIndex = 39;
+            textBox10.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(187, 219);
+            label8.Name = "label8";
+            label8.Size = new Size(59, 15);
+            label8.TabIndex = 38;
+            label8.Text = "창호 높이";
+            label8.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = Color.White;
+            textBox5.BorderStyle = BorderStyle.None;
+            textBox5.Location = new Point(265, 219);
+            textBox5.Name = "textBox5";
+            textBox5.ReadOnly = true;
+            textBox5.Size = new Size(100, 16);
+            textBox5.TabIndex = 37;
+            textBox5.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(431, 178);
+            label7.Name = "label7";
+            label7.Size = new Size(111, 15);
+            label7.TabIndex = 36;
+            label7.Text = "주변요소 음영 길이";
+            label7.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = Color.White;
+            textBox6.BorderStyle = BorderStyle.None;
+            textBox6.Location = new Point(561, 177);
+            textBox6.Name = "textBox6";
+            textBox6.ReadOnly = true;
+            textBox6.Size = new Size(100, 16);
+            textBox6.TabIndex = 34;
+            textBox6.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(443, 96);
+            label9.Name = "label9";
+            label9.Size = new Size(99, 15);
+            label9.TabIndex = 33;
+            label9.Text = "좌측면 돌출 길이";
+            // 
+            // textBox7
+            // 
+            textBox7.BackColor = Color.White;
+            textBox7.BorderStyle = BorderStyle.None;
+            textBox7.Location = new Point(561, 95);
+            textBox7.Name = "textBox7";
+            textBox7.ReadOnly = true;
+            textBox7.Size = new Size(100, 16);
+            textBox7.TabIndex = 31;
+            textBox7.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(455, 138);
+            label11.Name = "label11";
+            label11.Size = new Size(87, 15);
+            label11.TabIndex = 30;
+            label11.Text = "상부 돌출 길이";
+            label11.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // textBox8
+            // 
+            textBox8.BackColor = Color.White;
+            textBox8.BorderStyle = BorderStyle.None;
+            textBox8.Location = new Point(561, 137);
+            textBox8.Name = "textBox8";
+            textBox8.ReadOnly = true;
+            textBox8.Size = new Size(100, 16);
+            textBox8.TabIndex = 28;
+            textBox8.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(443, 55);
+            label13.Name = "label13";
+            label13.Size = new Size(99, 15);
+            label13.TabIndex = 27;
+            label13.Text = "우측면 돌출 길이";
+            // 
+            // textBox9
+            // 
+            textBox9.BackColor = Color.White;
+            textBox9.BorderStyle = BorderStyle.None;
+            textBox9.Location = new Point(561, 55);
+            textBox9.Name = "textBox9";
+            textBox9.ReadOnly = true;
+            textBox9.Size = new Size(100, 16);
+            textBox9.TabIndex = 25;
+            textBox9.TextAlign = HorizontalAlignment.Center;
             // 
             // textBox4
             // 
@@ -102,17 +272,17 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(447, 95);
+            label5.Location = new Point(135, 177);
             label5.Name = "label5";
-            label5.Size = new Size(124, 15);
+            label5.Size = new Size(111, 15);
             label5.TabIndex = 23;
-            label5.Text = "주변요소음영각도[γh]";
+            label5.Text = "주변요소 음영 각도";
             label5.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(683, 95);
+            label6.Location = new Point(371, 177);
             label6.Name = "label6";
             label6.Size = new Size(12, 15);
             label6.TabIndex = 22;
@@ -122,7 +292,7 @@
             // 
             textBox3.BackColor = Color.White;
             textBox3.BorderStyle = BorderStyle.None;
-            textBox3.Location = new Point(577, 94);
+            textBox3.Location = new Point(265, 176);
             textBox3.Name = "textBox3";
             textBox3.ReadOnly = true;
             textBox3.Size = new Size(100, 16);
@@ -132,11 +302,11 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(96, 95);
+            label3.Location = new Point(147, 95);
             label3.Name = "label3";
-            label3.Size = new Size(155, 15);
+            label3.Size = new Size(99, 15);
             label3.TabIndex = 20;
-            label3.Text = "좌측면돌출음영각도[γv,left]";
+            label3.Text = "좌측면 돌출 각도";
             // 
             // label4
             // 
@@ -161,17 +331,17 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(447, 55);
+            label1.Location = new Point(159, 137);
             label1.Name = "label1";
-            label1.Size = new Size(124, 15);
+            label1.Size = new Size(87, 15);
             label1.TabIndex = 17;
-            label1.Text = "상부돌출음영각도[γo]";
+            label1.Text = "상부 돌출 각도";
             label1.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(683, 55);
+            label2.Location = new Point(371, 137);
             label2.Name = "label2";
             label2.Size = new Size(12, 15);
             label2.TabIndex = 16;
@@ -181,7 +351,7 @@
             // 
             textBox1.BackColor = Color.White;
             textBox1.BorderStyle = BorderStyle.None;
-            textBox1.Location = new Point(577, 54);
+            textBox1.Location = new Point(265, 136);
             textBox1.Name = "textBox1";
             textBox1.ReadOnly = true;
             textBox1.Size = new Size(100, 16);
@@ -191,11 +361,11 @@
             // label67
             // 
             label67.AutoSize = true;
-            label67.Location = new Point(96, 55);
+            label67.Location = new Point(147, 54);
             label67.Name = "label67";
-            label67.Size = new Size(163, 15);
+            label67.Size = new Size(99, 15);
             label67.TabIndex = 14;
-            label67.Text = "우측면돌출음영각도[γv,right]";
+            label67.Text = "우측면 돌출 각도";
             // 
             // label68
             // 
@@ -216,18 +386,6 @@
             textBox23.Size = new Size(100, 16);
             textBox23.TabIndex = 12;
             textBox23.TextAlign = HorizontalAlignment.Center;
-            // 
-            // webView21
-            // 
-            webView21.AllowExternalDrop = true;
-            webView21.CreationProperties = null;
-            webView21.DefaultBackgroundColor = Color.White;
-            webView21.Dock = DockStyle.Bottom;
-            webView21.Location = new Point(3, 163);
-            webView21.Name = "webView21";
-            webView21.Size = new Size(786, 256);
-            webView21.TabIndex = 2;
-            webView21.ZoomFactor = 1D;
             // 
             // tabPage2
             // 
@@ -265,7 +423,6 @@
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
             tabPage1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
             tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)webView22).EndInit();
             ResumeLayout(false);
@@ -276,7 +433,6 @@
         private TabControl tabControl1;
         private TabPage tabPage1;
         private TabPage tabPage2;
-        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
         private Microsoft.Web.WebView2.WinForms.WebView2 webView22;
         private Label label5;
         private Label label6;
@@ -291,5 +447,19 @@
         private Label label68;
         private TextBox textBox23;
         private TextBox textBox4;
+        private Label label12;
+        private TextBox textBox11;
+        private Label label10;
+        private TextBox textBox10;
+        private Label label8;
+        private TextBox textBox5;
+        private Label label7;
+        private TextBox textBox6;
+        private Label label9;
+        private TextBox textBox7;
+        private Label label11;
+        private TextBox textBox8;
+        private Label label13;
+        private TextBox textBox9;
     }
 }
--- main/contents/3D/sub3dWinInfo.cs
+++ main/contents/3D/sub3dWinInfo.cs
@@ -20,15 +20,31 @@ namespace main.contents
         private void onVisibleChanged(object sender, EventArgs e)
         {
             String ID = main.MainContents.selID.Replace("board-", "");
-            string[][] rec = Program.DB.getValue(DB.type.ProjDB, "ZoneEnvelope_3D", "우측면돌출각도,좌측면돌출각도,상부돌출각도,주변요소음영각도,번호", "아이디 = '" + ID + "'");
+
+            ID = ID.Replace("_win1", "");
+            ID = ID.Replace("_win2", "");
+            ID = ID.Replace("_win3", "");
+            ID = ID.Replace("_win4", "");
+            ID = ID.Replace("_win5", "");
+
+            string[][] rec = Program.DB.getValue(DB.type.ProjDB, "ZoneEnvelope_3D", "존,우측면돌출각도,좌측면돌출각도,상부돌출각도,주변요소음영각도,우측면돌출길이,좌측면돌출길이,상부돌출길이,주변요소음영길이,번호", "아이디 = '" + ID + "'");
 
             if (rec.Length > 0)
             {
-                textBox23.Text = (rec[0][0] == "0" ? "0" : Double.Parse(rec[0][0]).ToString("#.##"));
-                textBox2.Text = (rec[0][1] == "0" ? "0" : Double.Parse(rec[0][1]).ToString("#.##"));
-                textBox1.Text = (rec[0][2] == "0" ? "0" : Double.Parse(rec[0][2]).ToString("#.##"));
-                textBox3.Text = (rec[0][3] == "0" ? "0" : Double.Parse(rec[0][3]).ToString("#.##"));
-                textBox4.Text = (rec[0][4]);
+                string[][] rec2 = Program.DB.getValue(DB.type.ProjDB, "ZoneGeneral_3D", "주광너비,주광깊이,상인방높이", "존번호 = '" + rec[0][0] + "'");
+
+                textBox23.Text = (rec[0][1] == "0" ? "0" : Double.Parse(rec[0][1]).ToString("#.##"));
+                textBox2.Text = (rec[0][2] == "0" ? "0" : Double.Parse(rec[0][2]).ToString("#.##"));
+                textBox1.Text = (rec[0][3] == "0" ? "0" : Double.Parse(rec[0][3]).ToString("#.##"));
+                textBox3.Text = (rec[0][4] == "0" ? "0" : Double.Parse(rec[0][4]).ToString("#.##"));
+                textBox9.Text = (rec[0][5] == "0" ? "0" : Double.Parse(rec[0][5]).ToString("#.##"));
+                textBox7.Text = (rec[0][6] == "0" ? "0" : Double.Parse(rec[0][6]).ToString("#.##"));
+                textBox8.Text = (rec[0][7] == "0" ? "0" : Double.Parse(rec[0][7]).ToString("#.##"));
+                textBox6.Text = (rec[0][8] == "0" ? "0" : Double.Parse(rec[0][8]).ToString("#.##"));
+                textBox10.Text = (rec2[0][0] == "0" ? "0" : Double.Parse(rec2[0][0]).ToString("#.##"));
+                textBox5.Text = (rec2[0][1] == "0" ? "0" : Double.Parse(rec2[0][1]).ToString("#.##"));
+                textBox11.Text = (rec2[0][2] == "0" ? "0" : Double.Parse(rec2[0][2]).ToString("#.##"));
+                textBox4.Text = (rec[0][9]);
             }
 
         }
