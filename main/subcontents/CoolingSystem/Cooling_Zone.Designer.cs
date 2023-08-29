d400499699fb3811f4babfd8cda8dbf71431b1b2
sungjungbark <139762551+sungjungbark@users.noreply.github.com>
2023-08-29 12:58:55 +0900

    냉방 관련 cs들 추가

---
+++ main/subcontents/CoolingSystem/Cooling_Zone.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace main.subcontents
+{
+    partial class Cooling_Zone
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle7 = new DataGridViewCellStyle();
+            button1 = new Button();
+            panel1 = new Panel();
+            CoolingZone_dataGridView = new DataGridView();
+            CoolingZoneList_panel = new DataGridView();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)CoolingZone_dataGridView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)CoolingZoneList_panel).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(760, 51);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "선택";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(CoolingZone_dataGridView);
+            panel1.Controls.Add(CoolingZoneList_panel);
+            panel1.Location = new Point(24, 80);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(811, 452);
+            panel1.TabIndex = 2;
+            // 
+            // CoolingZone_dataGridView
+            // 
+            CoolingZone_dataGridView.AllowUserToAddRows = false;
+            CoolingZone_dataGridView.AllowUserToDeleteRows = false;
+            CoolingZone_dataGridView.AllowUserToResizeColumns = false;
+            CoolingZone_dataGridView.AllowUserToResizeRows = false;        
+            CoolingZone_dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            CoolingZone_dataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            CoolingZone_dataGridView.BackgroundColor = Color.White;
+            CoolingZone_dataGridView.BorderStyle = BorderStyle.None;
+            CoolingZone_dataGridView.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            CoolingZone_dataGridView.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle2.Font = new Font("맑은 고딕", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = Color.White;
+            dataGridViewCellStyle2.SelectionForeColor = Color.Black;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            CoolingZone_dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            CoolingZone_dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            CoolingZone_dataGridView.Dock = DockStyle.Fill;
+            CoolingZone_dataGridView.Location = new Point(0, 0);
+            CoolingZone_dataGridView.Name = "CoolingZone_dataGridView";
+            CoolingZone_dataGridView.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = SystemColors.Control;
+            dataGridViewCellStyle3.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
+            CoolingZone_dataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            CoolingZone_dataGridView.RowHeadersVisible = false;
+            CoolingZone_dataGridView.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = Color.Black;
+            dataGridViewCellStyle4.SelectionBackColor = Color.White;
+            dataGridViewCellStyle4.SelectionForeColor = Color.Black;
+            CoolingZone_dataGridView.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            CoolingZone_dataGridView.RowTemplate.Height = 25;
+            CoolingZone_dataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            CoolingZone_dataGridView.Size = new Size(811, 452);
+            CoolingZone_dataGridView.TabIndex = 23;
+            CoolingZone_dataGridView.CellFormatting += CoolingZoneList_dataGridView_CellFormatting;
+            // 
+            // CoolingZoneList_panel
+            // 
+            CoolingZoneList_panel.AllowUserToAddRows = false;
+            CoolingZoneList_panel.AllowUserToDeleteRows = false;
+            CoolingZoneList_panel.AllowUserToResizeColumns = false;
+            CoolingZoneList_panel.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            CoolingZoneList_panel.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            CoolingZoneList_panel.BackgroundColor = SystemColors.ActiveCaption;
+            CoolingZoneList_panel.BorderStyle = BorderStyle.None;
+            CoolingZoneList_panel.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            CoolingZoneList_panel.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle5.Font = new Font("맑은 고딕", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle5.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle5.SelectionForeColor = Color.Black;
+            dataGridViewCellStyle5.WrapMode = DataGridViewTriState.True;
+            CoolingZoneList_panel.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            CoolingZoneList_panel.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            CoolingZoneList_panel.Dock = DockStyle.Fill;
+            CoolingZoneList_panel.Location = new Point(0, 0);
+            CoolingZoneList_panel.Name = "CoolingZoneList_panel";
+            dataGridViewCellStyle6.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = SystemColors.Control;
+            dataGridViewCellStyle6.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle6.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle6.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = DataGridViewTriState.True;
+            CoolingZoneList_panel.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            CoolingZoneList_panel.RowHeadersVisible = false;
+            CoolingZoneList_panel.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle7.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle7.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle7.ForeColor = Color.Black;
+            dataGridViewCellStyle7.SelectionBackColor = SystemColors.GradientInactiveCaption;
+            dataGridViewCellStyle7.SelectionForeColor = Color.Black;
+            CoolingZoneList_panel.RowsDefaultCellStyle = dataGridViewCellStyle7;
+            CoolingZoneList_panel.RowTemplate.Height = 25;
+            CoolingZoneList_panel.Size = new Size(811, 452);
+            CoolingZoneList_panel.TabIndex = 100;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(27, 22);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(52, 50);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // CoolingZoneList
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(856, 559);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel1);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "CoolingZoneList";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ZoneCooling";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)CoolingZone_dataGridView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)CoolingZoneList_panel).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button button1;
+        private Panel panel1;
+        private DataGridView CoolingZoneList_panel;
+        private PictureBox pictureBox1;
+        private DataGridView CoolingZone_dataGridView;
+    }
+}
---
+++ main/subcontents/CoolingSystem/Cooling_Zone.cs
@@ -0,0 +1,306 @@
+﻿using main.contents;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.ComponentModel.Design.ObjectSelectorEditor;
+
+namespace main.subcontents
+{
+    public partial class Cooling_Zone : Form
+    {
+
+        List<CoolingZone> CZS = new List<CoolingZone>();
+        string _SystemName;
+        string _SystemNum;
+
+        public Cooling_Zone(string Num, string Name)
+        {
+            InitializeComponent();
+            _SystemNum = Num;
+            _SystemName = Name;
+            string[][] Image = Program.DB.getValue(DB.type.BaseDB_HCneed, "메뉴아이콘", "하위메뉴아이콘", "하위메뉴명 = '냉방시스템'");
+            pictureBox1.Load(Program.gPath + Image[0][0]);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            //new StackedHeaderDecorator(CoolingZone_dataGridView, DataGridViewAutoSizeColumnsMode.Fill, dataGridView_RowHandle);
+
+
+            //실행함
+            List<string> CoolingZones = new List<string>();
+            coolingzone(coolingzonelist());
+            datagridviewShow();
+        }
+
+       
+        //dwd_mth
+        public string[][] coolingzonelist() //list 찾기
+        {
+            string[][] coolingzonename = Program.DB.getValue_SameCheck(DB.type.ProjDB, "Zone_HCneed",
+           "번호,이름,a", //값이있는 열
+           "난방_냉방='" + "냉방" + //조건1
+           "' AND 비이용일_이용일 = '" + "이용일" + //조건2
+             "'"); //마지막
+            return coolingzonename;
+        }
+        public void coolingzone(string[][] czl) //냉방존 보여주기
+        {
+            for (int i = 0; i < czl.Length; i++)
+            {
+                CoolingZone CZ = new CoolingZone();
+                CZ.Num = czl[i][0]; //존번호
+                CZ.Name = czl[i][1]; //존이름
+                CZ.Area = Convert.ToDouble(czl[i][2]);
+
+                string[][] coolingzone = Program.DB.getValue(DB.type.ProjDB, "Zone_HCneed",
+                  "Qcb_mth,theta_i,dwd_mth", //값이있는 열
+                    "난방_냉방= '" + "냉방" + //조건1
+                    "' AND 비이용일_이용일 = '" + "이용일" + //조건2
+                    "' AND 번호 = '" + czl[i][0] +  //조건3
+                    "'"); //마지막
+                for(int mth = 0;mth<12; mth++)
+                {
+                    CZ.Qcb_mth[mth] = Convert.ToDouble(coolingzone[mth][0]);
+                    CZ.theta_i[mth] = Convert.ToDouble(coolingzone[mth][0]);
+                    CZ.dwd_mth[mth] = Convert.ToDouble(coolingzone[mth][0]);
+                }
+                CZS.Add(CZ);
+            }
+           
+        }
+
+        private bool dataGridView_RowHandle(DataGridViewCell cell, int column, int row)
+        {
+            if (row % 2 == 0)
+            {
+                cell.Style.BackColor = Color.FromArgb(255, 255, 255);
+                return true;
+            }
+            else return false;
+        }
+        public void datagridviewShow()
+        {
+            CoolingZone_dataGridView.Rows.Clear();
+            DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn();
+            CoolingZone_dataGridView.Columns.Clear();
+            checkBoxColumn.HeaderText = "선택";
+            checkBoxColumn.Name = "check";
+            CoolingZone_dataGridView.Columns.Add(checkBoxColumn);
+
+            CoolingZone_dataGridView.Columns.Add("A1", "존번호");
+            CoolingZone_dataGridView.Columns.Add("A2", "존이름");
+            CoolingZone_dataGridView.Columns.Add("A3", "번호");
+            CoolingZone_dataGridView.Columns.Add("A4", "연간 정보.냉방에너지요구량.[kWh/m2·a]");
+            CoolingZone_dataGridView.Columns.Add("A5", "연간 정보.실내 평균온도.[℃]");
+            CoolingZone_dataGridView.Columns.Add("A6", "연간 정보.월 평균 이용일수.[d/mth]");
+            CoolingZone_dataGridView.Columns.Add("A7", "순바닥면적.[m2]");
+            CoolingZone_dataGridView.Columns.Add("A8", "최대냉방부하.[kW]");
+            CoolingZone_dataGridView.Columns[0].FillWeight = 50;
+            CoolingZone_dataGridView.Columns[1].FillWeight = 80;
+            CoolingZone_dataGridView.Columns[2].FillWeight = 50;
+            CoolingZone_dataGridView.Columns[3].FillWeight = 50;
+            CoolingZone_dataGridView.Columns[7].FillWeight = 80;
+            CoolingZone_dataGridView.Columns[8].FillWeight = 80;
+          
+            foreach (CoolingZone CZ in CZS) //모든존을 가지고 있음( 0, 3번만 별도 지정함 )
+            {
+                int nRow = CoolingZone_dataGridView.Rows.Add();
+                CoolingZone_dataGridView.Rows[nRow].Cells[1].Value = CZ.Num.ToString(); //존번호
+                CoolingZone_dataGridView.Rows[nRow].Cells[2].Value = CZ.Name.ToString(); //존이름
+                CoolingZone_dataGridView.Rows[nRow].Cells[4].Value = CZ.Qcb_a().ToString(); //
+                CoolingZone_dataGridView.Rows[nRow].Cells[5].Value = CZ.theta_i_ave().ToString();
+                CoolingZone_dataGridView.Rows[nRow].Cells[6].Value = CZ.dwd_mth_ave().ToString();
+                CoolingZone_dataGridView.Rows[nRow].Cells[7].Value = CZ.Area.ToString();
+                CoolingZone_dataGridView.Rows[nRow].Cells[8].Value = CZ.MaxLoad().ToString();
+            }
+
+
+            for (int k = 0; k < CoolingZone_dataGridView.Rows.Count; k++)
+            {
+                //냉방설비가 같은번호인것을 추출
+                string[][] _zc = Program.DB.getValue_SameCheck(DB.type.ProjDB, "CoolingZone", "번호", "존번호  = '" + CoolingZone_dataGridView.Rows[k].Cells[1].Value + "'"); //한개씩 검토 진행함
+                //같은번호가 있으면
+                if (_zc.Length > 0)
+                {
+                    CoolingZone_dataGridView.Rows[k].Cells[3].Value = _zc[0][0].ToString();//번호넣기
+
+                    if (_zc[0][0].ToString() == _SystemNum)
+                    {
+                        CoolingZone_dataGridView.Rows[k].Cells[0].Value = true;
+                    }
+                    else
+                    {
+                        CoolingZone_dataGridView.Rows[k].Cells[0].Value = false;
+                    }
+                }
+                else
+                {
+                    CoolingZone_dataGridView.Rows[k].Cells[3].Value = null;
+                    CoolingZone_dataGridView.Rows[k].Cells[0].Value = false;
+                }
+            }
+
+        }
+
+        // 데이터그리드 텍스트폼 지정
+        private void CoolingZoneList_dataGridView_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            for(int i =0; i < 5; i++)
+            {
+                int chk = 4+i;
+                if (e.ColumnIndex == chk && e.Value != null)
+                {
+                   string num = Convert.ToString(e.Value);
+                   double doubleNum;
+                   if (double.TryParse(num, out doubleNum))
+                   {
+                      e.Value = string.Format("{0:0.##}", doubleNum);
+                   }
+                   else
+                   {
+                       e.Value = num;
+                   }
+                  
+                }
+            }
+           
+        }
+        public void _sqlsave() //SQL에 저장하기
+        {
+            for (int k = 0; k < CoolingZone_dataGridView.Rows.Count; k++)
+            {
+
+                //냉방설비가 같은번호인것을 추출
+                string[][] _zc = Program.DB.getValue(DB.type.ProjDB, "CoolingZone", "번호,존번호", "존번호  = '" + CoolingZone_dataGridView.Rows[k].Cells[1].Value + "'"); //한개씩 검토 진행함
+                if (_zc.Length > 0 && _zc[0][0] == _SystemNum)
+                {
+                    Program.DB.deleteValue(DB.type.ProjDB, "CoolingZone", "존번호='" + CoolingZone_dataGridView.Rows[k].Cells[1].Value + "'");
+                }
+            }
+
+            for (int k = 0; k < CoolingZone_dataGridView.Rows.Count; k++) //sql에 저장하기
+            {
+                string[][] _zc1 = Program.DB.getValue(DB.type.ProjDB, "CoolingZone", "번호,명칭,존번호,존이름", "존번호  = '" + CoolingZone_dataGridView.Rows[k].Cells[1].Value + "'");
+                if (Convert.ToBoolean(CoolingZone_dataGridView.Rows[k].Cells[0].Value) == true)
+                {
+                    Program.DB.setValue(DB.type.ProjDB, "CoolingZone",
+                      "번호,명칭,존번호,존이름",
+                                "'" + 
+                                _SystemNum + "','" +
+                                _SystemName + "','" +
+                                CoolingZone_dataGridView.Rows[k].Cells[1].Value + "','" +
+                                CoolingZone_dataGridView.Rows[k].Cells[2].Value + "'", "존번호");
+                }
+                else if (Convert.ToBoolean(CoolingZone_dataGridView.Rows[k].Cells[0].Value) == false)
+                {
+                    if(_zc1.Length>0) 
+                    {
+                        Program.DB.setValue(DB.type.ProjDB, "CoolingZone",
+                     "번호,명칭,존번호,존이름",
+                               "'" +
+                             _zc1[0][0] + "','" +
+                             _zc1[0][1] +"','"  +
+                             _zc1[0][2] + "','" +
+                             _zc1[0][3] + "'", "존번호");
+                    }
+                    else
+                    {
+                        Program.DB.setValue(DB.type.ProjDB, "CoolingZone",
+                     "번호,명칭,존번호,존이름",
+                               "'" +
+                              CoolingZone_dataGridView.Rows[k].Cells[3].Value + "','" +
+                              "','" +
+                              CoolingZone_dataGridView.Rows[k].Cells[1].Value + "','" +
+                              CoolingZone_dataGridView.Rows[k].Cells[2].Value + "'", "존번호");
+                    }
+                }
+            }
+        }
+    
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _sqlsave();
+            string a = "OK";
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+
+
+    public class CoolingZone
+    {
+        public string Num, Name, SystemName;
+        public double Area; //MaxLoad 없음
+        private double _Qcb_a, _dwd_mth_ave, _theta_i_ave;
+        public double[] Qcb_mth = new double[12], dwd_mth = new double[12], theta_i = new double[12];
+
+        public double Qcb_a()
+        {
+            double j = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                j += Qcb_mth[i];
+            }
+            return j;
+        }
+        public double dwd_mth_ave()
+        {
+            double j = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                j += dwd_mth[i];
+            }
+            double k = j / 12;
+            return k;
+        }
+        public double theta_i_ave()
+        {
+            double j = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                j += theta_i[i];
+            }
+            double k = j / 12;
+            return k;
+        }
+
+        public double MaxLoad()
+        {
+            List<double> maxselect = new List<double>();
+            double k = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                double j = Qcb_mth[i];
+                maxselect.Add(j);
+            }
+            for (int i = 0; i < 12; i++)
+            {
+                if (maxselect.Max() == Qcb_mth[i])
+                {
+                    k = maxselect.Max() / dwd_mth[i] / 11;
+                    break;
+                }
+            }
+            return k;
+        }
+
+    } 
+}
+ 
+
+
+
+
