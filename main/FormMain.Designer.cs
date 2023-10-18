351cc214b114de3ef58881cff8e2ed07f9643eab
arcookie0 <arcookie0@gmail.com>
2023-10-18 18:03:30 +0900

    툴바영역 롤백 : 데모시연

--- main/FormMain.Designer.cs
+++ main/FormMain.Designer.cs
@@ -28,17 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             splitContainer1 = new SplitContainer();
-            toolStripContainer1 = new ToolStripContainer();
-            toolStrip1 = new ToolStrip();
-            toolStripButton1 = new ToolStripButton();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
             splitContainer1.SuspendLayout();
-            toolStripContainer1.ContentPanel.SuspendLayout();
-            toolStripContainer1.TopToolStripPanel.SuspendLayout();
-            toolStripContainer1.SuspendLayout();
-            toolStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // splitContainer1
@@ -53,80 +45,28 @@
             // splitContainer1.Panel2
             // 
             splitContainer1.Panel2.AutoScroll = true;
-            splitContainer1.Size = new Size(1259, 829);
+            splitContainer1.Size = new Size(1259, 861);
             splitContainer1.SplitterDistance = 325;
             splitContainer1.TabIndex = 0;
             splitContainer1.Resize += OnResize;
-            // 
-            // toolStripContainer1
-            // 
-            toolStripContainer1.BottomToolStripPanelVisible = false;
-            // 
-            // toolStripContainer1.ContentPanel
-            // 
-            toolStripContainer1.ContentPanel.Controls.Add(splitContainer1);
-            toolStripContainer1.ContentPanel.Size = new Size(1259, 829);
-            toolStripContainer1.Dock = DockStyle.Fill;
-            toolStripContainer1.LeftToolStripPanelVisible = false;
-            toolStripContainer1.Location = new Point(0, 0);
-            toolStripContainer1.Name = "toolStripContainer1";
-            toolStripContainer1.RightToolStripPanelVisible = false;
-            toolStripContainer1.Size = new Size(1259, 861);
-            toolStripContainer1.TabIndex = 0;
-            toolStripContainer1.Text = "toolStripContainer1";
-            // 
-            // toolStripContainer1.TopToolStripPanel
-            // 
-            toolStripContainer1.TopToolStripPanel.Controls.Add(toolStrip1);
-            toolStripContainer1.TopToolStripPanel.MinimumSize = new Size(0, 32);
-            // 
-            // toolStrip1
-            // 
-            toolStrip1.Dock = DockStyle.None;
-            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1 });
-            toolStrip1.Location = new Point(3, 0);
-            toolStrip1.Name = "toolStrip1";
-            toolStrip1.Padding = new Padding(4);
-            toolStrip1.Size = new Size(73, 31);
-            toolStrip1.TabIndex = 0;
-            // 
-            // toolStripButton1
-            // 
-            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
-            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
-            toolStripButton1.ImageScaling = ToolStripItemImageScaling.None;
-            toolStripButton1.ImageTransparentColor = Color.Magenta;
-            toolStripButton1.Name = "toolStripButton1";
-            toolStripButton1.Size = new Size(23, 20);
-            toolStripButton1.Text = "toolStripButton1";
             // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1259, 861);
-            Controls.Add(toolStripContainer1);
+            Controls.Add(splitContainer1);
             Name = "FormMain";
             Text = "FormMain";
             FormClosed += OnClosed;
             Load += FormMain_Load;
             ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
             splitContainer1.ResumeLayout(false);
-            toolStripContainer1.ContentPanel.ResumeLayout(false);
-            toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
-            toolStripContainer1.TopToolStripPanel.PerformLayout();
-            toolStripContainer1.ResumeLayout(false);
-            toolStripContainer1.PerformLayout();
-            toolStrip1.ResumeLayout(false);
-            toolStrip1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         public SplitContainer splitContainer1;
-        private ToolStripContainer toolStripContainer1;
-        private ToolStrip toolStrip1;
-        private ToolStripButton toolStripButton1;
     }
 }
