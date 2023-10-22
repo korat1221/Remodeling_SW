06cfe826b204b28f2d60a360bb3ba5ae244fdeb8
arcookie0 <arcookie0@gmail.com>
2023-10-22 22:41:43 +0900

    3D 인식 안정화

--- main/contents/3D/Model.cs
+++ main/contents/3D/Model.cs
@@ -21,6 +21,7 @@ using System.IO;
 using main.contents.Building;
 using main.subcontents.ConstructionWindow;
 using main.contents;
+using System.Diagnostics;
 
 namespace main.contents
 {
@@ -43,7 +44,8 @@ namespace main.contents
             SLInfo
         };
         Form[] forms = new Form[] { new sub3dZoneInfo(), new sub3dBridgeInfo(), new sub3dSpaceInfo(), new sub3dCWInfo(), new sub3dWLInfo(), new sub3dRFInfo(), new sub3dFRInfo(), new sub3dWINInfo(), new sub3dDRInfo(), new sub3dIWInfo(), new sub3dSLInfo() };
-
+        System.Windows.Forms.Timer tmBridge = new System.Windows.Forms.Timer();
+        string bridgeResult = "";
 
         public Model()
         {
@@ -65,6 +67,72 @@ namespace main.contents
             await webView21.EnsureCoreWebView2Async(null);
             webView21.CoreWebView2.WebMessageReceived += OnJSMessage;
             webView21.CoreWebView2.NavigationCompleted += OnNaviCompleted;
+
+            tmBridge.Interval = 200;
+            tmBridge.Tick += new EventHandler(tmBridgeProc);
+        }
+        void tmBridgeProc(object sender, EventArgs e)
+        {
+            tmBridge.Stop();
+            if (bridgeResult != "")
+            {
+                String s = bridgeResult;
+
+                bridgeResult = "";
+
+                try
+                {
+                    int n;
+
+                    if ((n = s.IndexOf("@@@")) >= 0)
+                    {
+                        String json = s.Substring(n + 3);
+                        Program.UTIL.write3DModel(Program.ProjName + ".json", json);
+                        Program.DB.executeSQL(DB.type.ProjDB, s.Substring(0, n));
+                        //             Program.UTIL.reloadWebCtrl();
+
+                        string[][] Win = Program.DB.getValue(DB.type.ProjDB, "ZoneEnvelope_3D", "아이디", "외피유형 = '창호'");
+                        if (Win.Length > 0)
+                        {
+                            for (int k = 0; k < Win.Length; k++)
+                            {
+                                ZoneShade zoneshade = new ZoneShade(Win[k][0]);
+                                //zoneshade.Calc_방위각();
+                                //MessageBox.Show(zoneshade.태양우측방위각[1].ToString());
+                                zoneshade.Calc_방위각();
+                                zoneshade.Calc_지형물음영();
+
+                                zoneshade.Calc_상부음영();
+                                zoneshade.Calc_좌측음영();
+                                zoneshade.Calc_우측음영();
+                                zoneshade.Calc_음영계수();
+
+                                //for (int i = 0; i < 12; i++)
+                                //{
+                                //    MessageBox.Show(zoneshade.상부돌출음영길이좌[i].ToString());
+                                //}
+
+                                //MessageBox.Show(zoneshade.지형물수평음영길이[1].ToString());
+                                //zoneshade.Calc_상부음영();
+                                //zoneshade.Calc_좌측음영();
+                                //zoneshade.Calc_우측음영();
+                                //zoneshade.Calc_음영계수();
+                                zoneshade.Save();
+                            }
+                        }
+                        resetZoneDraw();
+                        Program.UTIL.loadMainMenu(2);
+                        Program.DB.saveProject();
+                    }
+                    else
+                    {
+                        Program.UTIL.selectWall(s);
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
         }
         public void DoLoadForm(int idx)
         {
@@ -79,61 +147,9 @@ namespace main.contents
 
         void OnJSMessage(object sender, CoreWebView2WebMessageReceivedEventArgs args)
         {
-            try
-            {
-                int n;
-                String s = args.TryGetWebMessageAsString();
-
-                if ((n = s.IndexOf("@@@")) >= 0)
-                {
-                    String json = s.Substring(n + 3);
-                    Program.UTIL.write3DModel(Program.ProjName + ".json", json);
-                    Program.DB.executeSQL(DB.type.ProjDB, s.Substring(0, n));
-       //             Program.UTIL.reloadWebCtrl();
-
-                    string[][] Win = Program.DB.getValue(DB.type.ProjDB, "ZoneEnvelope_3D", "아이디", "외피유형 = '창호'");
-                    if(Win.Length> 0)
-                    {
-                        for(int k = 0 ; k < Win.Length; k++)
-                        {
-                            ZoneShade zoneshade = new ZoneShade(Win[k][0]);
-                            //zoneshade.Calc_방위각();
-                            //MessageBox.Show(zoneshade.태양우측방위각[1].ToString());
-                            zoneshade.Calc_방위각();
-                            zoneshade.Calc_지형물음영();
-
-                            zoneshade.Calc_상부음영();
-                            zoneshade.Calc_좌측음영();
-                            zoneshade.Calc_우측음영();
-                            zoneshade.Calc_음영계수();
-
-                            //for (int i = 0; i < 12; i++)
-                            //{
-                            //    MessageBox.Show(zoneshade.상부돌출음영길이좌[i].ToString());
-                            //}
-
-                            //MessageBox.Show(zoneshade.지형물수평음영길이[1].ToString());
-                            //zoneshade.Calc_상부음영();
-                            //zoneshade.Calc_좌측음영();
-                            //zoneshade.Calc_우측음영();
-                            //zoneshade.Calc_음영계수();
-                            zoneshade.Save();
-                        }                       
-                    }
-                    resetZoneDraw();
-                    Program.UTIL.loadMainMenu(2);
-                    Program.DB.saveProject();
-                }
-                else
-                {
-                    Program.UTIL.selectWall(s);
-                }
-            }
-            catch (Exception ex)
-            {
-                int i;
-                i = 0;
-            }
+            tmBridge.Stop();
+            bridgeResult = args.TryGetWebMessageAsString();
+            tmBridge.Start();
         }
         private void resetZoneDraw()
         {
