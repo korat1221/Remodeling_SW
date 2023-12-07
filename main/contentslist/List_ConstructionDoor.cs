07603334c9544a66e32af3aff3c7a910e503064c
ipazbesohee <135787145+ipazbesohee@users.noreply.github.com>
2023-12-07 12:00:26 +0900

    외부출입문 리스트

---
+++ main/contentslist/List_ConstructionDoor.cs
@@ -0,0 +1,188 @@
+﻿using main.contents;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace main.contentslist
+{
+    public partial class List_ConstructionDoor : Form
+    {
+        static String currentID = "";
+        static String inEditing = "Add";
+
+        String WallNum;
+        double CountDB;
+        int SelectRow;
+        //DataTable WallList = new DataTable();
+        DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn();
+
+        public List_ConstructionDoor()
+        {
+            InitializeComponent();
+
+            string[][] Image = Program.DB.getValue(DB.type.BaseDB_HCneed, "메뉴아이콘", "하위메뉴아이콘", "하위메뉴명 = '외부출입문'");
+            Icon_pictureBox.Load(Program.gPath + Image[0][0]);
+            Icon_pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            Program.DB.initTable(DB.type.ProjDB, "ConstructionWall");
+            Create_Table();
+        }
+
+        private void GeneralPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Panel p = (Panel)sender;
+            ControlPaint.DrawBorder(e.Graphics, p.DisplayRectangle, Color.FromArgb(153, 180, 209), ButtonBorderStyle.Solid);
+        }
+
+
+        private void Add_button_Click(object sender, EventArgs e)
+        {
+            WallNum = Program.UTIL.CreateNum("ConstructionWall", "번호", "DR");
+
+            Program.getMenuForm().ResetForm(3);
+
+            Load_form(WallNum, "Add");
+        }
+
+        public static bool OnLoadProc(Form form)
+        {
+            ConstructionWall f = (ConstructionWall)form;
+
+            if (inEditing == "Edit")
+            {
+                f.LoadData(currentID);
+
+            }
+            else if (inEditing == "Copy")
+            {
+                f.LoadData(currentID);
+            }
+            else
+            {
+                f.ResetForm(currentID);
+            }
+
+            return true;
+        }
+
+        private void Load_form(String ID, String editing)
+        {
+            currentID = ID;
+            inEditing = editing;
+            Program.getMenuForm().DoLoadForm(7, OnLoadProc);
+        }
+
+
+        public void Create_Table()
+        {
+            new StackedHeaderDecorator(dataGridView1, DataGridViewAutoSizeColumnsMode.Fill, datagridviewDesign);
+            dataGridView1.Columns.Clear();
+            checkBoxColumn.HeaderText = "선택";
+            checkBoxColumn.Name = "check";
+            dataGridView1.Columns.Add(checkBoxColumn);
+            dataGridView1.Columns.Add("A1", "번호");
+            dataGridView1.Columns.Add("A2", "명칭");
+            dataGridView1.Columns.Add("A3", "Type");
+            dataGridView1.Columns.Add("A4", "유효열관류율.[W/m²·K]");
+            dataGridView1.Columns.Add("A5", "흡수율.[-]");
+            dataGridView1.Columns.Add("A6", "면적.[m²]");
+            dataGridView1.Columns[0].Width = 40;
+        }
+
+        private Boolean datagridviewDesign(DataGridViewCell cell, int column, int row)
+        {
+            if (row % 2 == 1)
+            {
+                cell.Style.BackColor = SystemColors.InactiveBorder;
+                cell.Style.ForeColor = Color.Black;
+                cell.Style.SelectionBackColor = SystemColors.InactiveBorder;
+                cell.Style.SelectionForeColor = Color.Black;
+                return true;
+            }
+            else
+            {
+                cell.Style.BackColor = Color.FromArgb(255, 255, 255);
+                cell.Style.ForeColor = Color.Black;
+                cell.Style.SelectionBackColor = Color.FromArgb(255, 255, 255);
+                cell.Style.SelectionForeColor = Color.Black;
+                return true;
+            }
+        }
+        public void load_List()
+        {
+            string[][] List = Program.DB.getValue(DB.type.ProjDB, "ConstructionWall", "번호,명칭,Type,유효열관류율,흡수율", "");
+            List<object> mainMenu = new List<object>(); // 예시 코드: 메인 메뉴 동적 할당
+            String Blank = "";
+            // WallList.Rows.Clear();
+            dataGridView1.Rows.Clear();
+            for (int n = 0; n < List.Length; n++)
+            {
+                dataGridView1.Rows.Add();
+                int nRow = dataGridView1.Rows.Count - 1;
+                dataGridView1.Rows[nRow].Cells[1].Value = List[n][0];
+                dataGridView1.Rows[nRow].Cells[2].Value = List[n][1];
+                dataGridView1.Rows[nRow].Cells[3].Value = List[n][2];
+                dataGridView1.Rows[nRow].Cells[4].Value = String.Format("{0:F2}", Convert.ToDouble(List[n][3]));
+                dataGridView1.Rows[nRow].Cells[5].Value = String.Format("{0:F2}", Convert.ToDouble(List[n][4]));
+                dataGridView1.Rows[nRow].Cells[6].Value = null;
+                // WallList.Rows.Add(List[n][0], List[n][1], List[n][2], String.Format("{0:F2}", Convert.ToDouble(List[n][3])), String.Format("{0:F2}", Convert.ToDouble(List[n][4])));
+                mainMenu.Add(new { text = List[n][0] + "." + List[n][1], id = "{\\\"formID\\\":7,\\\"ID\\\":\\\"" + List[n][0] + "\\\"}" }); // 예시 코드: 메인 메뉴 동적 할당
+            }
+            //dataGridView1.DataSource = WallList;
+            CountDB = List.Length;
+            Program.UTIL.resetMainTree(1, 5, mainMenu.ToArray(), "51"); // 예시 코드: 메인 메뉴 동적 할당
+        }
+
+        private void Remove_button_Click(object sender, EventArgs e)
+        {
+            int k = dataGridView1.CurrentCell.RowIndex;
+            if ((MessageBox.Show(dataGridView1.Rows[k].Cells[2].Value.ToString() + "을 삭제 하시겠습니까?", "삭제 확인", MessageBoxButtons.YesNo) == DialogResult.Yes))
+            {
+                if (k > -1)
+                {
+                    String Delete_Num = dataGridView1.Rows[k].Cells[1].Value.ToString();
+                    Program.DB.deleteValue(DB.type.ProjDB, "ConstructionWall", "번호 ='" + Delete_Num + "'");
+                    load_List();
+
+                }
+            }
+
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            int k = dataGridView1.CurrentCell.RowIndex;
+            if (k > -1)
+            {
+                Load_form(dataGridView1.Rows[k].Cells[1].Value.ToString(), "Edit");
+
+            }
+
+        }
+
+        private void Copy_button_Click(object sender, EventArgs e)
+        {
+            WallNum = Program.UTIL.CreateNum("ConstructionWall", "번호", "DR");
+            int k = dataGridView1.CurrentCell.RowIndex;
+            if (k > -1)
+            {
+                String Copy_Num = dataGridView1.Rows[k].Cells[1].Value.ToString();
+
+                Program.DB.CopyValue(DB.type.ProjDB, "ConstructionWall", "번호 ='" + Copy_Num + "'", WallNum);
+                Program.DB.executeSQL(DB.type.ProjDB, "UPDATE  ConstructionWall" + " SET 명칭 = '" + dataGridView1.Rows[k].Cells[2].Value.ToString() + "_복사" + "' WHERE  번호 = '" + WallNum + "'");
+                Load_form(WallNum, "Copy");
+
+            }
+        }
+
+        public void LoadData(String ID)            // 리스트에서 항목 더블 클릭시 - 뷰를 ID 의 getValue 값으로 채우기
+        {
+            load_List();
+        }
+    }
+}
