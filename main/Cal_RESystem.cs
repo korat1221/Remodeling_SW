22aeba8745d75a26a3bbfc0e7a6739a35e854bf1
korat1221 <108967572+korat1221@users.noreply.github.com>
2024-01-29 16:23:21 +0900

    신재생List 추가

---
+++ main/Cal_RESystem.cs
@@ -0,0 +1,138 @@
+﻿using main.contents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main
+{
+
+    internal class Cal_RESystem
+    {
+        public string Num;
+        #region 태양광시스템
+        public double PVPpk_kW; //태양광 최대출력 
+        public double[] Qf_elec = new double[12]; //월별 전기소요량 
+        public double[] PVEelpvoutm_kWh = new double[12]; //월별 전기생산량 
+        public string PVType; //계통연계유형 
+
+        private string PVBatteryNumber, BatteryType;
+        private double Cnenm; //배터리 정격 용량 
+        private double ηDoD, ηBatt; //배터리 최대 방전 깊이, 배터리 시스템효율
+        private double[] γQ = new double[12];//배터리 규격에 대한 지수(소요량 대비 최대성능 계수)
+        private double Ceff; //배터리 용량(배터리 타입에 따른 방전 깊이 고려) 
+        private double[] CQ = new double[12];//배터리 규격에 대한 지수 (소요량 대비 배터리 용량 계수)
+        public double[] fmatch = new double[12];//매칭계수
+        private double[] fBatt = new double[12]; //배터리 수정계수 
+        public double[] Qbatt_loss = new double[12];// 배터리 손실
+
+        public double[] Qf_nutz_linked = new double[12];//계통연계형 월별 태양광사용량 
+        public double[] Qf_nutz_nonlinked = new double[12]; //독립형 월별 태양광사용량 
+        public double[] Qf_nutz_PV = new double[12]; //최종 월별 태양광사용량 
+        #endregion
+        public Cal_RESystem(string Num) { this.Num = Num; }
+
+        public void Load_PVdata()
+        {
+            string[][] Value;
+            for (int mth = 0; mth < 12; mth++)
+            {
+                Value = Program.DB.getValue(DB.type.ProjDB, "PV_Result", "최대성능,전기생산량", "번호='" + Num + "' And 월 ='" + (mth + 1).ToString() + "월'");
+                if (Value.Length > 0)
+                {
+                    PVPpk_kW = Convert.ToDouble(Value[0][0]);
+                    PVEelpvoutm_kWh[mth] = Convert.ToDouble(Value[0][1]);
+                }
+            }
+            Value = Program.DB.getValue(DB.type.ProjDB, "PV_Form", "계통유형", "번호='" + Num + "'");
+            if (Value.Length > 0)
+            {
+                PVType = Value[0][0];
+            }
+            String[][] Battery = Program.DB.getValue(DB.type.ProjDB, "PV_Form", "배터리번호,배터리용량", "번호='" + Num + "'");
+            if (Battery.Length > 0)
+            {
+                PVBatteryNumber = Battery[0][0];
+                Cnenm = Convert.ToDouble(Battery[0][1]);
+            }
+
+            Battery = Program.DB.getValue(DB.type.ProjDB, "User_PVBattery", "배터리타입", "번호 ='" + PVBatteryNumber + "'");
+            if (Battery.Length > 0)
+            {
+                BatteryType = Battery[0][0];
+            }
+            Battery = Program.DB.getValue(DB.type.BaseDB_RESystem, "태양광배터리계수", "최대방전깊이,시스템효율", "배터리타입 ='" + BatteryType + "'");
+            if (Battery.Length > 0)
+            {
+                ηDoD = Convert.ToDouble(Battery[0][0]);
+                ηBatt =Convert.ToDouble(Battery[0][1]);
+            }
+
+        }
+
+        public void Cal_Qf_elec()
+        {
+            string[][] Final;
+            string[][]  Value = Program.DB.getValue(DB.type.ProjDB, "PV_Result", "최대성능", "월 ='1월'");
+            double PVPpk_kW_total = 0;
+            if(Value.Length > 0)
+            {
+                for(int n=0; n<Value.Length; n++)
+                {
+                    PVPpk_kW_total += Convert.ToDouble(Value[n][0]);
+                }
+            }
+            for (int mth = 0; mth < 12; mth++)
+            {
+                Final = Program.DB.getValue(DB.type.ProjDB, "FinalEnergy_Result", "총에너지소요량", "연료='전기' And 월 ='" + (mth + 1).ToString() + "월'");
+                if (Final.Length > 0)
+                {
+                    Qf_elec[mth] = Convert.ToDouble(Final[0][0]) * PVPpk_kW / PVPpk_kW_total;
+                }
+            }
+
+        }
+        public void Cal_Battery()
+        {
+            Ceff = Cnenm * ηDoD;
+            for(int mth = 0;mth < 12; mth++)
+            {
+                CQ[mth] = Ceff / Qf_elec[mth] * 100;
+                γQ[mth] = PVPpk_kW / Qf_elec[mth] * 100;
+                fBatt[mth] = Math.Max(1, (0.2 * Math.Log(γQ[mth], Math.E) + 1.85) * Math.Pow(CQ[mth] , (0.1 * Math.Log(γQ[mth], Math.E) + 0.25)));
+                
+            }            
+        }
+
+        public void Cal_fmatch()
+        {
+            double[] x = new double[12];
+            for(int mth =0; mth < 12;mth++)
+            {
+                x[mth] = PVEelpvoutm_kWh[mth] / Qf_elec[mth];
+                fmatch[mth] = (x[mth] + 1 / x[mth] - 1) / (x[mth] + 1 / x[mth]);
+            }
+        }
+
+        public void Cal_Qf_pv()
+        {
+            for(int mth = 0; mth < 12;mth++)
+            {
+                Qf_nutz_linked[mth]= fmatch[mth]* PVEelpvoutm_kWh[mth];
+                Qbatt_loss[mth] = Qf_nutz_linked[mth] * (1 - ηBatt) * (fBatt[mth] - 1);
+                Qf_nutz_nonlinked[mth] = Math.Max(Qf_nutz_linked[mth], Math.Min(PVEelpvoutm_kWh[mth], Qf_nutz_linked[mth] * fBatt[mth]) - Qbatt_loss[mth]);
+                if(PVType== "독립형")
+                {
+                    Qf_nutz_PV[mth] = Qf_nutz_nonlinked[mth]; 
+                }
+                else //계통연계형 
+                {
+                    Qf_nutz_PV[mth] = Qf_nutz_linked[mth];
+                }
+            }
+        }
+
+    }  
+}
---
+++ main/contentslist/List_FuelCell.cs
@@ -0,0 +1,171 @@
+﻿using main.contents;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace main.contentslist
+{
+    public partial class List_FuelCell : Form
+    {
+        static String currentID = "";
+        static String inEditing = "Add";
+
+        String Num;
+        double CountDB;
+        int SelectRow;
+        // DataTable ListTable = new DataTable();
+        DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn();
+
+        public List_FuelCell()
+        {
+            InitializeComponent();
+
+            Icon_pictureBox.Load(Program.gPath + "images/2ndicon/6_2FuelCell.png");
+            Icon_pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            Create_Table();
+        }
+
+        private void GeneralPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Panel p = (Panel)sender;
+            ControlPaint.DrawBorder(e.Graphics, p.DisplayRectangle, Color.FromArgb(153, 180, 209), ButtonBorderStyle.Solid);
+        }
+
+        public static bool OnLoadProc(Form form)
+        {
+            FuelCell f = (FuelCell)form;
+
+            if (inEditing == "Edit")
+            {
+                f.LoadData(currentID);
+
+            }
+            else if (inEditing == "Copy")
+            {
+                f.LoadData(currentID);
+            }
+            else
+            {
+                f.ResetForm(currentID);
+            }
+
+            return true;
+        }
+
+        private void Load_form(String ID, String editing)
+        {
+            currentID = ID;
+            inEditing = editing;
+            Program.getMenuForm().DoLoadForm(22, OnLoadProc);
+        }
+
+
+        public void Create_Table()
+        {
+            new StackedHeaderDecorator(dataGridView1, DataGridViewAutoSizeColumnsMode.Fill, datagridviewDesign);
+            dataGridView1.Columns.Clear();
+            checkBoxColumn.HeaderText = "선택";
+            checkBoxColumn.Name = "check";
+            dataGridView1.Columns.Add(checkBoxColumn);
+            dataGridView1.Columns.Add("A1", "유형");
+            dataGridView1.Columns.Add("A2", "용량.[kW]");
+            dataGridView1.Columns.Add("A3", "효율.[%]");
+            dataGridView1.Columns[0].Width = 40;
+        }
+
+        private Boolean datagridviewDesign(DataGridViewCell cell, int column, int row)
+        {
+            if (row % 2 == 1)
+            {
+                cell.Style.BackColor = SystemColors.InactiveBorder;
+                cell.Style.ForeColor = Color.Black;
+                cell.Style.SelectionBackColor = SystemColors.InactiveBorder;
+                cell.Style.SelectionForeColor = Color.Black;
+                return true;
+            }
+            else
+            {
+                cell.Style.BackColor = Color.FromArgb(255, 255, 255);
+                cell.Style.ForeColor = Color.Black;
+                cell.Style.SelectionBackColor = Color.FromArgb(255, 255, 255);
+                cell.Style.SelectionForeColor = Color.Black;
+                return true;
+            }
+        }
+        public void load_List()
+        {
+            string[][] Value = Program.DB.getValue(DB.type.ProjDB, "HeatingSystem_Form", "번호,명칭", "");
+            List<object> mainMenu = new List<object>(); // 예시 코드: 메인 메뉴 동적 할당
+            List<object> subMenu = new List<object>(); // 예시 코드: 메인 메뉴 동적 할당
+            for (int n = 0; n < Value.Length; n++)
+            {
+                subMenu.Add(new { text = Value[n][0] + "." + Value[n][1], id = "{\\\"formID\\\":22,\\\"ID\\\":\\\"" + Value[n][0] + "\\\"}" }); // 예시 코드: 메인 메뉴 동적 할당
+            }
+            mainMenu.Add(new { text = "연료전지", id = "{\\\"formID\\\":22,\\\"ID\\\":\\\"SOLAR_2\\\"}" }); // 예시 코드: 메인 메뉴 동적 할당
+
+            CountDB = Value.Length;        
+        }
+        private void Add_button_Click(object sender, EventArgs e)
+        {
+            Num = Program.UTIL.CreateNum("HeatingSystem_Form", "번호", "HS");
+
+            Program.getMenuForm().ResetForm(22);
+
+            Load_form(Num, "Add");
+        }
+
+        private void Remove_button_Click(object sender, EventArgs e)
+        {
+            int k = dataGridView1.CurrentCell.RowIndex;
+            if ((MessageBox.Show(dataGridView1.Rows[k].Cells[2].Value.ToString() + "을 삭제 하시겠습니까?", "삭제 확인", MessageBoxButtons.YesNo) == DialogResult.Yes))
+            {
+                if (k > -1)
+                {
+                    String Delete_Num = dataGridView1.Rows[k].Cells[1].Value.ToString();
+                    Program.DB.deleteValue(DB.type.ProjDB, "HeatingSystem_Form", "번호 ='" + Delete_Num + "'");
+                    load_List();
+
+                }
+            }
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            int k = dataGridView1.CurrentCell.RowIndex;
+            if (k > -1)
+            {
+                Load_form(dataGridView1.Rows[k].Cells[1].Value.ToString(), "Edit");
+            }
+
+        }
+
+        private void Copy_button_Click(object sender, EventArgs e)
+        {
+            Num = Program.UTIL.CreateNum("HeatingSystem_Form", "번호", "HS");
+            int k = dataGridView1.CurrentCell.RowIndex;
+            if (k > -1)
+            {
+                String Copy_Num = dataGridView1.Rows[k].Cells[1].Value.ToString();
+
+                Program.DB.CopyValue(DB.type.ProjDB, "HeatingSystem_Form", "번호 ='" + Copy_Num + "'", Num);
+                Program.DB.executeSQL(DB.type.ProjDB, "UPDATE  HeatingSystem_Form" + " SET 명칭 = '" + dataGridView1.Rows[k].Cells[2].Value.ToString() + "_복사" + "' WHERE  번호 = '" + Num + "'");
+                Load_form(Num, "Copy");
+
+            }
+        }
+
+        public void LoadData(String ID)            // 리스트에서 항목 더블 클릭시 - 뷰를 ID 의 getValue 값으로 채우기
+        {
+            load_List();
+        }
+
+       
+    }
+}
