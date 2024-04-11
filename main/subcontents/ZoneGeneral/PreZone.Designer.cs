dd39755633be3acfed3f7e65ffa8d62c998d283e
korat1221 <108967572+korat1221@users.noreply.github.com>
2024-04-11 14:10:30 +0900

    기존존 선택폼

---
+++ main/subcontents/ZoneGeneral/PreZone.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace main.subcontents.ZoneGeneral
+{
+    partial class PreZone
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            GeneralPanel = new Panel();
+            label3 = new Label();
+            Save_button = new Button();
+            Zone_dataGridView = new DataGridView();
+            GeneralPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)Zone_dataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // GeneralPanel
+            // 
+            GeneralPanel.BackColor = Color.AliceBlue;
+            GeneralPanel.Controls.Add(label3);
+            GeneralPanel.Location = new Point(0, -2);
+            GeneralPanel.Name = "GeneralPanel";
+            GeneralPanel.Size = new Size(916, 47);
+            GeneralPanel.TabIndex = 18;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(20, 13);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 16);
+            label3.TabIndex = 103;
+            label3.Text = "기존 존 정보";
+            // 
+            // Save_button
+            // 
+            Save_button.BackColor = SystemColors.ButtonHighlight;
+            Save_button.ForeColor = Color.Black;
+            Save_button.Location = new Point(768, 442);
+            Save_button.Name = "Save_button";
+            Save_button.Size = new Size(135, 25);
+            Save_button.TabIndex = 20;
+            Save_button.Text = "SAVE";
+            Save_button.UseVisualStyleBackColor = true;
+            Save_button.Click += Save_button_Click;
+            // 
+            // Zone_dataGridView
+            // 
+            Zone_dataGridView.AllowUserToAddRows = false;
+            Zone_dataGridView.AllowUserToDeleteRows = false;
+            Zone_dataGridView.AllowUserToResizeColumns = false;
+            Zone_dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            Zone_dataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            Zone_dataGridView.BackgroundColor = SystemColors.Control;
+            Zone_dataGridView.BorderStyle = BorderStyle.None;
+            Zone_dataGridView.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            Zone_dataGridView.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle1.Font = new Font("맑은 고딕", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle1.SelectionForeColor = Color.Black;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            Zone_dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            Zone_dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            Zone_dataGridView.Location = new Point(0, 39);
+            Zone_dataGridView.Name = "Zone_dataGridView";
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            Zone_dataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            Zone_dataGridView.RowHeadersVisible = false;
+            Zone_dataGridView.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.GradientInactiveCaption;
+            dataGridViewCellStyle3.SelectionForeColor = Color.Black;
+            Zone_dataGridView.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            Zone_dataGridView.RowTemplate.Height = 25;
+            Zone_dataGridView.Size = new Size(916, 397);
+            Zone_dataGridView.TabIndex = 19;
+            // 
+            // PreZone
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(915, 479);
+            Controls.Add(Zone_dataGridView);
+            Controls.Add(Save_button);
+            Controls.Add(GeneralPanel);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "PreZone";
+            Text = "Previous Zone";
+            GeneralPanel.ResumeLayout(false);
+            GeneralPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)Zone_dataGridView).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel GeneralPanel;
+        private Button Save_button;
+        private DataGridView Zone_dataGridView;
+        private TextBox textBox2;
+        private TextBox d_ins_textBox;
+        private Label label3;
+    }
+}
---
+++ main/subcontents/ZoneGeneral/PreZone.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace main.subcontents.ZoneGeneral
+{
+    public partial class PreZone : Form
+    {
+        double Count_DB;
+        ArrayList SelectRow = new ArrayList(); ArrayList SelectZone_split = new ArrayList();
+        String ZoneNum, Layer;
+        public string SelectZone;
+        string[][] PreProjNum = Program.DB.getValue(DB.type.ProjDB, "BuildingGeneral", "기존프로젝트", "");
+        public PreZone(String Num, String SelectZone_nonsplit, string Layer)
+        {
+            InitializeComponent();
+            ZoneNum = Num;
+            this.Layer = Layer;
+            load_table_DB();
+            if (SelectZone_nonsplit != null)
+            {
+                Load_SaveValue(SelectZone_nonsplit);
+            }
+            Load_SelectZone();
+        }
+
+        void load_table_DB()
+        {
+            //  DataTable table_Zone = new DataTable();
+            new StackedHeaderDecorator(Zone_dataGridView, DataGridViewAutoSizeColumnsMode.Fill, datagridviewDesign);
+            DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn();
+            Zone_dataGridView.Columns.Clear();
+            checkBoxColumn.HeaderText = "선택";
+            checkBoxColumn.Name = "check";
+            Zone_dataGridView.Columns.Add(checkBoxColumn);
+
+            Zone_dataGridView.Columns.Add("A1", "번호");
+            Zone_dataGridView.Columns.Add("A2", "존명칭");
+            Zone_dataGridView.Columns.Add("A3", "용도프로필");
+            Zone_dataGridView.Columns.Add("A4", "냉난방");
+            Zone_dataGridView.Columns.Add("A5", "연간 요구량.난방.[kWh/a]");
+            Zone_dataGridView.Columns.Add("A6", "연간 요구량.냉방.[kWh/a]");
+            Zone_dataGridView.Columns.Add("A7", "면적.[m²]");
+            Zone_dataGridView.Columns.Add("A8", "리모델링 후");
+            Zone_dataGridView.Columns[0].Width = 40;
+            Zone_dataGridView.Columns[5].Width = 70;
+            Zone_dataGridView.Columns[6].Width = 70;
+            Zone_dataGridView.Columns[7].Width = 60;
+
+            string[][] Value = Program.DB.querySQL(PreProjNum[0][0], "Select DISTINCT a.존번호 From ZoneGeneral_Form as a Inner Join ZoneEnvelope_3D as b on a.존번호 = b.존 where b.층 ='" + Layer + "'");
+            if (Value.Length > 0)
+            {
+                int[] Zone_순서 = new int[Value.Length]; string[] Zone_순서2 = new string[Value.Length];
+                for (int n = 0; n < Value.Length; n++)
+                {
+                    Zone_순서[n] = Convert.ToInt32(Value[n][0].Substring(Value[n][0].Length - 3, 3));
+                }
+                Array.Sort(Zone_순서);
+                for (int n = 0; n < Zone_순서.Length; n++)
+                {
+                    for (int k = 0; k < Value.Length; k++)
+                    { 
+                        if (Zone_순서[n] == Convert.ToInt32(Value[k][0].Substring(Value[k][0].Length - 3, 3)))
+                        {
+                            Zone_순서2[n] = Value[k][0];
+                        }
+                    }
+                }
+                for (int n = 0; n < Zone_순서2.Length; n++)
+                {
+                    Value = Program.DB.querySQL(PreProjNum[0][0], "Select 존번호,존이름,용도프로필,순바닥면적,냉난방유무 From ZoneGeneral_Form where 존번호='" + Zone_순서2[n] + "'");
+
+                    string[][] 요구량 = Program.DB.getValue(PreProjNum[0][0], "Zone_HCneed_Result", "Qb_a", "번호 ='" + Zone_순서2[n] + "' AND 난방_냉방 = '난방'");
+                    string[][] 요구량2 = Program.DB.getValue(PreProjNum[0][0], "Zone_HCneed_Result", "Qb_a", "번호 ='" + Zone_순서2[n] + "' AND 난방_냉방 = '냉방'");
+
+                    Zone_dataGridView.Rows.Add();
+                    int nRow = Zone_dataGridView.Rows.Count - 1;
+                    Zone_dataGridView.Rows[nRow].Cells[1].Value = Value[0][0];
+
+                    Zone_dataGridView.Rows[nRow].Cells[2].Value = Value[0][1];
+                    Zone_dataGridView.Rows[nRow].Cells[3].Value = Value[0][2];
+                    Zone_dataGridView.Rows[nRow].Cells[4].Value = Value[0][4];
+                    if (요구량.Length > 0)
+                    {
+                        Zone_dataGridView.Rows[nRow].Cells[5].Value = string.Format("{0:F2}", Convert.ToDouble(요구량[0][0]));
+                        Zone_dataGridView.Rows[nRow].Cells[6].Value = string.Format("{0:F2}", Convert.ToDouble(요구량2[0][0]));
+                    }
+                    Zone_dataGridView.Rows[nRow].Cells[7].Value = string.Format("{0:F1}", Convert.ToDouble(Value[0][3]));
+
+                }
+            }
+        }
+
+
+        private Boolean datagridviewDesign(DataGridViewCell cell, int column, int row)
+        {
+            if (row % 2 == 1)
+            {
+                cell.Style.BackColor = SystemColors.InactiveBorder;
+                cell.Style.ForeColor = Color.Black;
+                cell.Style.SelectionBackColor = SystemColors.InactiveBorder;
+                cell.Style.SelectionForeColor = Color.Black;
+                return true;
+            }
+            else
+            {
+                cell.Style.BackColor = Color.FromArgb(255, 255, 255);
+                cell.Style.ForeColor = Color.Black;
+                cell.Style.SelectionBackColor = Color.FromArgb(255, 255, 255);
+                cell.Style.SelectionForeColor = Color.Black;
+                return true;
+            }
+        }
+        private void SelectCheckBox()
+        {
+            SelectRow.Clear();
+            foreach (DataGridViewRow row in Zone_dataGridView.Rows)
+            {
+                if (Convert.ToBoolean(row.Cells["check"].Value))
+                {
+                    row.DefaultCellStyle.SelectionBackColor = SystemColors.GradientInactiveCaption;
+                    SelectRow.Add(row.Index);
+                }
+            }
+        }
+
+        private void Save_button_Click(object sender, EventArgs e)
+        {
+            SelectCheckBox();
+            for (int k = 0; k < SelectRow.Count; k++)
+            {
+                if (k == SelectRow.Count - 1)
+                {
+                    this.SelectZone += Zone_dataGridView.Rows[Convert.ToInt16(SelectRow[k])].Cells[1].Value.ToString();
+                }
+                else
+                {
+                    this.SelectZone += Zone_dataGridView.Rows[Convert.ToInt16(SelectRow[k])].Cells[1].Value.ToString() + "+";
+                }
+            }
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+        private void reset()
+        {
+            SelectRow.Clear();
+            SelectZone_split.Clear();
+            this.SelectZone = null;
+
+            for (int n = 0; n < Zone_dataGridView.Rows.Count; n++)
+            {
+                Zone_dataGridView.Rows[n].Cells[0].Value = false;
+            }
+        }
+
+        private void Load_SelectZone()
+        {
+            ArrayList Zone_split = new ArrayList();
+            string[][] value = Program.DB.getValue(DB.type.ProjDB, "ZoneGeneral_Form", "존번호,존이름,기존존", "");
+            if (value.Length > 0)
+            {
+                for (int i = 0; i < value.Length; i++)
+                {
+                    string[] token = value[i][2].Split('+');
+                    Zone_split.Clear();
+                    foreach (var item in token)
+                    {
+                        Zone_split.Add(item.ToString());
+                    }
+                    for (int k = 0; k < Zone_split.Count; k++)
+                    {
+                        for (int n = 0; n < Zone_dataGridView.Rows.Count; n++)
+                        {
+                            if (Zone_dataGridView.Rows[n].Cells[1].Value.ToString() == Zone_split[k].ToString())
+                            {
+                                if (Zone_dataGridView.Rows[n].Cells[8].Value == null)
+                                { Zone_dataGridView.Rows[n].Cells[8].Value = value[i][0] + "." + value[i][1]; }
+                                else
+                                {
+                                    Zone_dataGridView.Rows[n].Cells[8].Value = Zone_dataGridView.Rows[n].Cells[8].Value.ToString() + ", " + value[i][0] + "." + value[i][1];
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void Load_SaveValue(String SelectZone_nonsplit)
+        {
+            reset();
+            string[] token = SelectZone_nonsplit.Split('+');
+            SelectZone_split.Clear();
+            foreach (var item in token)
+            {
+                SelectZone_split.Add(item.ToString());
+            }
+            for (int k = 0; k < SelectZone_split.Count; k++)
+            {
+                for (int n = 0; n < Zone_dataGridView.Rows.Count; n++)
+                {
+                    if (Zone_dataGridView.Rows[n].Cells[1].Value.ToString() == SelectZone_split[k].ToString())
+                    {
+                        Zone_dataGridView.Rows[n].Cells[0].Value = true;
+                    }
+                }
+            }
+        }
+    }
+
+}
+
