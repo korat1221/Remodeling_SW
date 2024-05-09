b699c53e3f4efc562fef384571385cd0d6577f5f
korat1221 <108967572+korat1221@users.noreply.github.com>
2024-05-09 13:41:33 +0900

    급탕HP DB 폼 추가

---
+++ main/subcontents/DHWSystem/DHWHP_DB.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace main.subcontents.DHWSystem
+{
+    partial class DHWHP_DB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            GeneralPanel = new Panel();
+            label4 = new Label();
+            Icon_pictureBox = new PictureBox();
+            Save_button = new Button();
+            HP_dataGridView = new DataGridView();
+            GeneralPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)Icon_pictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)HP_dataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // GeneralPanel
+            // 
+            GeneralPanel.BackColor = Color.AliceBlue;
+            GeneralPanel.Controls.Add(label4);
+            GeneralPanel.Controls.Add(Icon_pictureBox);
+            GeneralPanel.Location = new Point(0, -2);
+            GeneralPanel.Name = "GeneralPanel";
+            GeneralPanel.Size = new Size(800, 74);
+            GeneralPanel.TabIndex = 18;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("맑은 고딕", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(68, 32);
+            label4.Name = "label4";
+            label4.Size = new Size(83, 15);
+            label4.TabIndex = 103;
+            label4.Text = "급탕 히트펌프";
+            // 
+            // Icon_pictureBox
+            // 
+            Icon_pictureBox.Location = new Point(12, 14);
+            Icon_pictureBox.Name = "Icon_pictureBox";
+            Icon_pictureBox.Size = new Size(50, 50);
+            Icon_pictureBox.TabIndex = 102;
+            Icon_pictureBox.TabStop = false;
+            // 
+            // Save_button
+            // 
+            Save_button.BackColor = SystemColors.ButtonHighlight;
+            Save_button.ForeColor = Color.Black;
+            Save_button.Location = new Point(365, 442);
+            Save_button.Name = "Save_button";
+            Save_button.Size = new Size(135, 25);
+            Save_button.TabIndex = 20;
+            Save_button.Text = "SAVE";
+            Save_button.UseVisualStyleBackColor = true;
+            Save_button.Click += Save_button_Click;
+            // 
+            // HP_dataGridView
+            // 
+            HP_dataGridView.AllowUserToAddRows = false;
+            HP_dataGridView.AllowUserToDeleteRows = false;
+            HP_dataGridView.AllowUserToResizeColumns = false;
+            HP_dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            HP_dataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            HP_dataGridView.BackgroundColor = SystemColors.Control;
+            HP_dataGridView.BorderStyle = BorderStyle.None;
+            HP_dataGridView.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            HP_dataGridView.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle1.Font = new Font("맑은 고딕", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle1.SelectionForeColor = Color.Black;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            HP_dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            HP_dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            HP_dataGridView.Location = new Point(0, 68);
+            HP_dataGridView.Name = "HP_dataGridView";
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            HP_dataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            HP_dataGridView.RowHeadersVisible = false;
+            HP_dataGridView.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.GradientInactiveCaption;
+            dataGridViewCellStyle3.SelectionForeColor = Color.Black;
+            HP_dataGridView.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            HP_dataGridView.RowTemplate.Height = 25;
+            HP_dataGridView.Size = new Size(593, 364);
+            HP_dataGridView.TabIndex = 19;
+            // 
+            // DHWHP_DB
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(592, 479);
+            Controls.Add(HP_dataGridView);
+            Controls.Add(Save_button);
+            Controls.Add(GeneralPanel);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "DHWHP_DB";
+            Text = "DHWHP_DB";
+            GeneralPanel.ResumeLayout(false);
+            GeneralPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)Icon_pictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)HP_dataGridView).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel GeneralPanel;
+        private Button Save_button;
+        private DataGridView HP_dataGridView;
+        private TextBox textBox2;
+        private TextBox d_ins_textBox;
+        private Label label4;
+        private PictureBox Icon_pictureBox;
+    }
+}
---
+++ main/subcontents/DHWSystem/DHWHP_DB.cs
@@ -0,0 +1,165 @@
+﻿using main.contents;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace main.subcontents.DHWSystem
+{
+    public partial class DHWHP_DB : Form
+    {
+        ArrayList SelectRow = new ArrayList(); ArrayList SelectHP_split = new ArrayList();
+        public string SelectHP;
+        public string HC, Carrier;
+
+        public DHWHP_DB( String SelectHP_nonsplit)
+        {
+            InitializeComponent();
+
+            string[][] Image = Program.DB.getValue(DB.type.BaseDB_HCneed, "메뉴아이콘", "하위메뉴아이콘", "하위메뉴명 = '장비일람표'");
+            if (Image.Length > 0)
+            {
+                Icon_pictureBox.Load(Program.gPath + Image[0][0]);
+                Icon_pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            create_table();
+            load_table_DB();
+            if (SelectHP_nonsplit != null)
+            {
+                Load_SaveValue(SelectHP_nonsplit);
+            }
+        }
+              
+
+        void create_table()
+        {
+            HP_dataGridView.Columns.Clear();
+            new StackedHeaderDecorator(HP_dataGridView, DataGridViewAutoSizeColumnsMode.Fill, datagridviewDesign);
+            DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn();
+            checkBoxColumn.HeaderText = "선택";
+            checkBoxColumn.Name = "check";
+            HP_dataGridView.Columns.Add(checkBoxColumn);
+
+            HP_dataGridView.Columns.Add("A1", "번호");
+            HP_dataGridView.Columns.Add("A2", "명칭");
+            HP_dataGridView.Columns.Add("A3", "용량" + Environment.NewLine + "[kW]");
+            HP_dataGridView.Columns.Add("A4", "COP" + Environment.NewLine + "[kW]");
+            HP_dataGridView.Columns.Add("A5", "소비전력" + Environment.NewLine + "[kW]");
+            HP_dataGridView.Columns[0].Width = 40;
+        }
+        void load_table_DB()
+        {           
+            HP_dataGridView.Rows.Clear();
+            string[][] User_Value = Program.DB.getValue(DB.type.ProjDB, "User_DHWHP", "번호,명칭,급탕정격용량,급탕정격COP,급탕정격소비전력", "");
+            if (User_Value.Length > 0)
+            {
+                for (int n = 0; n < User_Value.Length; n++)
+                {
+
+                    HP_dataGridView.Rows.Add();
+                    int nRow = HP_dataGridView.Rows.Count - 1;
+                    HP_dataGridView.Rows[nRow].Cells[1].Value = User_Value[n][0];
+                    HP_dataGridView.Rows[nRow].Cells[2].Value = User_Value[n][1];
+                    HP_dataGridView.Rows[nRow].Cells[3].Value = string.Format("{0:F1}", Convert.ToDouble(User_Value[n][2]));
+                    HP_dataGridView.Rows[nRow].Cells[4].Value = string.Format("{0:F1}", Convert.ToDouble(User_Value[n][3]));
+                    HP_dataGridView.Rows[nRow].Cells[5].Value = string.Format("{0:F1}", Convert.ToDouble(User_Value[n][4]));
+                }
+            }
+        }
+
+        private Boolean datagridviewDesign(DataGridViewCell cell, int column, int row)
+        {
+            if (row % 2 == 1)
+            {
+                cell.Style.BackColor = SystemColors.InactiveBorder;
+                cell.Style.ForeColor = Color.Black;
+                cell.Style.SelectionBackColor = SystemColors.InactiveBorder;
+                cell.Style.SelectionForeColor = Color.Black;
+                return true;
+            }
+            else
+            {
+                cell.Style.BackColor = Color.FromArgb(255, 255, 255);
+                cell.Style.ForeColor = Color.Black;
+                cell.Style.SelectionBackColor = Color.FromArgb(255, 255, 255);
+                cell.Style.SelectionForeColor = Color.Black;
+                return true;
+            }
+        }
+
+        private void SelectCheckBox()
+        {
+            foreach (DataGridViewRow row in HP_dataGridView.Rows)
+            {
+                if (Convert.ToBoolean(row.Cells["check"].Value))
+                {
+                    row.DefaultCellStyle.SelectionBackColor = SystemColors.GradientInactiveCaption;
+                    SelectRow.Add(row.Index);
+                }
+            }
+        }
+
+        private void Save_button_Click(object sender, EventArgs e)
+        {
+            SelectRow.Clear();
+            SelectCheckBox();
+            for (int k = 0; k < SelectRow.Count; k++)
+            {
+                if (k == SelectRow.Count - 1)
+                {
+                    SelectHP += HP_dataGridView.Rows[Convert.ToInt16(SelectRow[k])].Cells[1].Value.ToString();
+                }
+                else
+                {
+                    SelectHP += HP_dataGridView.Rows[Convert.ToInt16(SelectRow[k])].Cells[1].Value.ToString() + "+";
+                }
+            }
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+        private void reset()
+        {
+            SelectRow.Clear();
+            SelectHP_split.Clear();
+            SelectHP = null;
+
+            for (int n = 0; n < HP_dataGridView.Rows.Count; n++)
+            {
+                HP_dataGridView.Rows[n].Cells[0].Value = false;
+            }
+
+        }
+        private void Load_SaveValue(String SelectHP_nonsplit)
+        {
+            reset();
+            string[] token = SelectHP_nonsplit.Split('+');
+            SelectHP_split.Clear();
+            foreach (var item in token)
+            {
+                SelectHP_split.Add(item.ToString());
+            }
+            for (int k = 0; k < SelectHP_split.Count; k++)
+            {
+                for (int n = 0; n < HP_dataGridView.Rows.Count; n++)
+                {
+                    if (HP_dataGridView.Rows[n].Cells[1].Value.ToString() == SelectHP_split[k].ToString())
+                    {
+                        HP_dataGridView.Rows[n].Cells[0].Value = true;
+                    }
+                }
+            }
+        }
+
+    }
+}
