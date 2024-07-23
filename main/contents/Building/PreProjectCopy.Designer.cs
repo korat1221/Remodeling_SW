5c7faa5c646e3f95906520c1d024da1f25fa77cb
korat1221 <108967572+korat1221@users.noreply.github.com>
2024-07-23 11:41:40 +0900

    기존 프로젝트 카피 폼 추가

---
+++ main/contents/Building/PreProjectCopy.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace main.contents
+{
+    partial class PreProjectCopy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            panel1 = new Panel();
+            label4 = new Label();
+            System_checkBox = new CheckBox();
+            System_pictureBox = new PictureBox();
+            Zone_checkBox = new CheckBox();
+            Zone_pictureBox = new PictureBox();
+            Model_checkBox = new CheckBox();
+            Model_pictureBox = new PictureBox();
+            Construction_checkBox = new CheckBox();
+            Construction_pictureBox = new PictureBox();
+            Building_checkBox = new CheckBox();
+            Building_pictureBox = new PictureBox();
+            Save_button = new Button();
+            dataGridView1 = new DataGridView();
+            chk = new DataGridViewCheckBoxColumn();
+            num = new DataGridViewTextBoxColumn();
+            pnum = new DataGridViewTextBoxColumn();
+            pname = new DataGridViewTextBoxColumn();
+            type = new DataGridViewTextBoxColumn();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)System_pictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Zone_pictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Model_pictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Construction_pictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Building_pictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(System_checkBox);
+            panel1.Controls.Add(System_pictureBox);
+            panel1.Controls.Add(Zone_checkBox);
+            panel1.Controls.Add(Zone_pictureBox);
+            panel1.Controls.Add(Model_checkBox);
+            panel1.Controls.Add(Model_pictureBox);
+            panel1.Controls.Add(Construction_checkBox);
+            panel1.Controls.Add(Construction_pictureBox);
+            panel1.Controls.Add(Building_checkBox);
+            panel1.Controls.Add(Building_pictureBox);
+            panel1.Controls.Add(Save_button);
+            panel1.Controls.Add(dataGridView1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(713, 360);
+            panel1.TabIndex = 19;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("맑은 고딕", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(12, 21);
+            label4.Name = "label4";
+            label4.Size = new Size(111, 15);
+            label4.TabIndex = 130;
+            label4.Text = "기존 건물 프로젝트";
+            // 
+            // System_checkBox
+            // 
+            System_checkBox.AutoSize = true;
+            System_checkBox.Enabled = false;
+            System_checkBox.Location = new Point(520, 260);
+            System_checkBox.Name = "System_checkBox";
+            System_checkBox.Size = new Size(78, 19);
+            System_checkBox.TabIndex = 129;
+            System_checkBox.Text = "설비 정보";
+            System_checkBox.UseVisualStyleBackColor = true;
+            System_checkBox.CheckedChanged += CheckBox_CheckedChanged;
+            // 
+            // System_pictureBox
+            // 
+            System_pictureBox.Location = new Point(524, 186);
+            System_pictureBox.Name = "System_pictureBox";
+            System_pictureBox.Size = new Size(70, 60);
+            System_pictureBox.TabIndex = 128;
+            System_pictureBox.TabStop = false;
+            // 
+            // Zone_checkBox
+            // 
+            Zone_checkBox.AutoSize = true;
+            Zone_checkBox.Enabled = false;
+            Zone_checkBox.Location = new Point(422, 260);
+            Zone_checkBox.Name = "Zone_checkBox";
+            Zone_checkBox.Size = new Size(66, 19);
+            Zone_checkBox.TabIndex = 127;
+            Zone_checkBox.Text = "존 정보";
+            Zone_checkBox.UseVisualStyleBackColor = true;
+            Zone_checkBox.CheckedChanged += CheckBox_CheckedChanged;
+            // 
+            // Zone_pictureBox
+            // 
+            Zone_pictureBox.Location = new Point(420, 186);
+            Zone_pictureBox.Name = "Zone_pictureBox";
+            Zone_pictureBox.Size = new Size(70, 60);
+            Zone_pictureBox.TabIndex = 126;
+            Zone_pictureBox.TabStop = false;
+            // 
+            // Model_checkBox
+            // 
+            Model_checkBox.AutoSize = true;
+            Model_checkBox.Enabled = false;
+            Model_checkBox.Location = new Point(316, 260);
+            Model_checkBox.Name = "Model_checkBox";
+            Model_checkBox.Size = new Size(70, 19);
+            Model_checkBox.TabIndex = 125;
+            Model_checkBox.Text = "3D 정보";
+            Model_checkBox.UseVisualStyleBackColor = true;
+            Model_checkBox.CheckedChanged += CheckBox_CheckedChanged;
+            // 
+            // Model_pictureBox
+            // 
+            Model_pictureBox.Location = new Point(316, 186);
+            Model_pictureBox.Name = "Model_pictureBox";
+            Model_pictureBox.Size = new Size(70, 60);
+            Model_pictureBox.TabIndex = 124;
+            Model_pictureBox.TabStop = false;
+            // 
+            // Construction_checkBox
+            // 
+            Construction_checkBox.AutoSize = true;
+            Construction_checkBox.Enabled = false;
+            Construction_checkBox.Location = new Point(202, 260);
+            Construction_checkBox.Name = "Construction_checkBox";
+            Construction_checkBox.Size = new Size(90, 19);
+            Construction_checkBox.TabIndex = 123;
+            Construction_checkBox.Text = "구조체 정보";
+            Construction_checkBox.UseVisualStyleBackColor = true;
+            Construction_checkBox.CheckedChanged += CheckBox_CheckedChanged;
+            // 
+            // Construction_pictureBox
+            // 
+            Construction_pictureBox.Location = new Point(212, 186);
+            Construction_pictureBox.Name = "Construction_pictureBox";
+            Construction_pictureBox.Size = new Size(70, 60);
+            Construction_pictureBox.TabIndex = 122;
+            Construction_pictureBox.TabStop = false;
+            // 
+            // Building_checkBox
+            // 
+            Building_checkBox.AutoSize = true;
+            Building_checkBox.Location = new Point(104, 260);
+            Building_checkBox.Name = "Building_checkBox";
+            Building_checkBox.Size = new Size(78, 19);
+            Building_checkBox.TabIndex = 121;
+            Building_checkBox.Text = "건물 정보";
+            Building_checkBox.UseVisualStyleBackColor = true;
+            Building_checkBox.CheckedChanged += CheckBox_CheckedChanged;
+            // 
+            // Building_pictureBox
+            // 
+            Building_pictureBox.Location = new Point(108, 186);
+            Building_pictureBox.Name = "Building_pictureBox";
+            Building_pictureBox.Size = new Size(70, 60);
+            Building_pictureBox.TabIndex = 120;
+            Building_pictureBox.TabStop = false;
+            // 
+            // Save_button
+            // 
+            Save_button.BackColor = SystemColors.ButtonHighlight;
+            Save_button.ForeColor = Color.Black;
+            Save_button.Location = new Point(524, 315);
+            Save_button.Name = "Save_button";
+            Save_button.Size = new Size(135, 25);
+            Save_button.TabIndex = 119;
+            Save_button.Text = "SAVE";
+            Save_button.UseVisualStyleBackColor = true;
+            Save_button.Click += Save_button_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.BackgroundColor = SystemColors.Window;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle1.Font = new Font("맑은 고딕", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle1.SelectionForeColor = Color.Black;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { chk, num, pnum, pname, type });
+            dataGridView1.Location = new Point(12, 39);
+            dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.GradientInactiveCaption;
+            dataGridViewCellStyle3.SelectionForeColor = Color.Black;
+            dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(678, 135);
+            dataGridView1.TabIndex = 100;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // chk
+            // 
+            chk.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+            chk.FillWeight = 91.37056F;
+            chk.HeaderText = "";
+            chk.Name = "chk";
+            chk.Resizable = DataGridViewTriState.False;
+            chk.Width = 24;
+            // 
+            // num
+            // 
+            num.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+            num.FillWeight = 106.2909F;
+            num.HeaderText = "번호";
+            num.Name = "num";
+            num.ReadOnly = true;
+            num.Resizable = DataGridViewTriState.False;
+            num.Width = 60;
+            // 
+            // pnum
+            // 
+            pnum.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+            pnum.FillWeight = 22.2307777F;
+            pnum.HeaderText = "프로젝트 번호";
+            pnum.Name = "pnum";
+            pnum.ReadOnly = true;
+            pnum.Resizable = DataGridViewTriState.False;
+            pnum.Width = 110;
+            // 
+            // pname
+            // 
+            pname.FillWeight = 257.877F;
+            pname.HeaderText = "프로젝트명";
+            pname.Name = "pname";
+            // 
+            // type
+            // 
+            type.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+            type.FillWeight = 22.2307777F;
+            type.HeaderText = "유형";
+            type.Name = "type";
+            type.ReadOnly = true;
+            type.Resizable = DataGridViewTriState.False;
+            type.Width = 96;
+            // 
+            // PreProjectCopy
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.AliceBlue;
+            ClientSize = new Size(710, 358);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "PreProjectCopy";
+            Text = "기존 건물 프로젝트 복사";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)System_pictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Zone_pictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Model_pictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Construction_pictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Building_pictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private TextBox textBox2;
+        private TextBox d_ins_textBox;
+        private Panel panel1;
+        private CheckBox System_checkBox;
+        private PictureBox System_pictureBox;
+        private CheckBox Zone_checkBox;
+        private PictureBox Zone_pictureBox;
+        private CheckBox Model_checkBox;
+        private PictureBox Model_pictureBox;
+        private CheckBox Construction_checkBox;
+        private PictureBox Construction_pictureBox;
+        private CheckBox Building_checkBox;
+        private PictureBox Building_pictureBox;
+        private Button Save_button;
+        private DataGridView dataGridView1;
+        private Label label4;
+        private DataGridViewCheckBoxColumn chk;
+        private DataGridViewTextBoxColumn num;
+        private DataGridViewTextBoxColumn pnum;
+        private DataGridViewTextBoxColumn pname;
+        private DataGridViewTextBoxColumn type;
+    }
+}
---
+++ main/contents/Building/PreProjectCopy.cs
@@ -0,0 +1,238 @@
+﻿using main.contents;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace main.contents
+{
+    public partial class PreProjectCopy : Form
+    {
+        public string pid0;
+        public List<string> tables = new List<string>();
+        public bool model_copy = false;
+
+        public class ComboboxItem
+        {
+            public string Text { get; set; }
+            public object Value { get; set; }
+
+            public override string ToString()
+            {
+                return Text;
+            }
+        }
+
+        public PreProjectCopy()
+        {
+            InitializeComponent();
+            Building_pictureBox.Load(Program.gPath + "images/1sticon/1.Building_on.png");
+            Building_pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            Construction_pictureBox.Load(Program.gPath + "images/1sticon/2.Construction_on.png");
+            Construction_pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            Model_pictureBox.Load(Program.gPath + "images/1sticon/3.3D_on.png");
+            Model_pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            Zone_pictureBox.Load(Program.gPath + "images/1sticon/4.Zone_on.png");
+            Zone_pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            System_pictureBox.Load(Program.gPath + "images/1sticon/5.System_on.png");
+            System_pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            drawList();
+        }
+        private void drawList()
+        {
+            dataGridView1.Rows.Clear();
+            string[][] res = Program.DB.querySQL(DB.type.ProjListDB, "SELECT ID, pnum, title, type FROM projects WHERE type='1'");
+            if (res.Length > 0)
+            {
+                for (int n = 0; n < res.Length; n++)
+                {
+                    int nRow = dataGridView1.Rows.Add();
+                    for (int k = 0; k < 4; k++)
+                    {
+                        if (k == 3) { dataGridView1.Rows[nRow].Cells[k + 1].Value = "기존건물"; }
+                        else { dataGridView1.Rows[nRow].Cells[k + 1].Value =res[n][k]; }
+                    }
+
+                    DataGridViewCheckBoxCell cell = dataGridView1.Rows[nRow].Cells[0] as DataGridViewCheckBoxCell;
+
+                }
+            }
+
+        }
+        private void CheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            int k = GetSelectedIndex();
+            if(k < 0)
+            {
+                MessageBox.Show("먼저 복사할 프로젝트부터 선택하세요.");
+            }
+            else
+            {
+                Construction_checkBox.Enabled = Building_checkBox.Checked;
+                if (!Building_checkBox.Checked)
+                {
+                    Construction_checkBox.Checked = false;
+                }
+
+                Model_checkBox.Enabled = Construction_checkBox.Checked;
+                if (!Construction_checkBox.Checked)
+                {
+                    Model_checkBox.Checked = false;
+                }
+
+                Zone_checkBox.Enabled = Model_checkBox.Checked;
+                if (!Model_checkBox.Checked)
+                {
+                    Zone_checkBox.Checked = false;
+                }
+
+                System_checkBox.Enabled = Zone_checkBox.Checked;
+                if (!Zone_checkBox.Checked)
+                {
+                    System_checkBox.Checked = false;
+                }
+            }            
+        }
+        private int GetSelectedIndex()
+        {
+            for (int k = 0; k < dataGridView1.Rows.Count; k++)
+            {
+                if (Convert.ToBoolean(dataGridView1.Rows[k].Cells[0].Value) == true)
+                {
+                    return k;
+                }
+            }
+            return -1;
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                dataGridView1.CommitEdit(DataGridViewDataErrorContexts.Commit);
+
+                for (int k = 0; k < dataGridView1.Rows.Count; k++)
+                {
+                    if (k != dataGridView1.CurrentCell.RowIndex)
+                    {
+                        dataGridView1.Rows[k].Cells[0].Value = false;
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[k].Cells[0].Value = true;
+                    }
+
+                }
+            }
+
+        }
+
+        private void Save_button_Click(object sender, EventArgs e)
+        {
+            int k = GetSelectedIndex();
+            if (k < 0)
+            {
+                MessageBox.Show("먼저 복사할 프로젝트부터 선택하세요.");
+            }
+            else
+            {
+                pid0 = dataGridView1.Rows[k].Cells[2].Value.ToString();
+                if (Building_checkBox.Checked)
+                {
+                    tables.Add("BuildingGeneral");
+                    tables.Add("BuildingEnergyUse");
+                    tables.Add("BlowDoorTest");
+                    tables.Add("User_PVModule");
+                    tables.Add("User_PVInverter");
+                    tables.Add("User_PVBattery");
+                    tables.Add("User_FC");
+                    tables.Add("User_WP");
+                    tables.Add("User_Boiler");
+                    tables.Add("User_AirHP");
+                    tables.Add("User_GroundHP");
+                    tables.Add("User_GroundWHP");
+                    tables.Add("User_Pump");
+                    tables.Add("User_ce");
+                    tables.Add("User_Solar");
+                    tables.Add("User_ABS");
+                    tables.Add("User_DH");
+                    tables.Add("User_AHU");
+                    tables.Add("User_HRV");
+                    tables.Add("User_DHWHP");
+                    tables.Add("User_AirCooler");
+                    tables.Add("User_WaterCooler");
+                    tables.Add("User_CoolerTop");
+                }
+                if (Construction_checkBox.Checked)
+                {
+                    tables.Add("ConstructionCW");
+                    tables.Add("ConstructionWall");
+                    tables.Add("ConstructionRoof");
+                    tables.Add("ConstructionFloor");
+                    tables.Add("ConstructionWindow");
+                    tables.Add("ConstructionDoor");
+                    tables.Add("ConstructionBlind");
+                    tables.Add("SubWindow");
+                    tables.Add("Import_WindowSize");
+                    tables.Add("Import_CWSize");
+                    tables.Add("User_WindowFrame");
+                    tables.Add("User_CWFrame");
+                    tables.Add("User_Glass");
+                    tables.Add("User_DoubleGlass");
+                    tables.Add("User_WindowSpacer");
+                    tables.Add("User_CWSpacer");
+                    tables.Add("User_CWDoorFrame");
+                    tables.Add("User_WindowInstall");
+                    tables.Add("User_CWInstall");
+                    tables.Add("User_Material");
+                    tables.Add("User_Blind");
+                    tables.Add("User_DoorInstall");
+                }
+                if (Model_checkBox.Checked)
+                {
+                    model_copy = true;
+                    tables.Add("Blind_3D");
+                    tables.Add("Shade_3D");
+                    tables.Add("ZoneGeneral_3D");
+                    tables.Add("ZoneEnvelope_3D");
+                    tables.Add("ThermalBridge_3D");
+                    tables.Add("User_TB");
+                }
+                if (Zone_checkBox.Checked)
+                {
+                    tables.Add("User_Lighting");
+                    tables.Add("User_Renew");
+                    tables.Add("ZoneGeneral_Form");
+                    tables.Add("ZoneLighting_form");
+                }
+                if (System_checkBox.Checked)
+                {
+                    tables.Add("HeatingSystem_Form");
+                    tables.Add("Heating_ce_Form");
+                    tables.Add("CoolingSystem_Form");
+                    tables.Add("Cooling_ce_Form");
+                    tables.Add("DHWSystem_Form");
+                    tables.Add("AHUSystem_Form");
+                    tables.Add("PV_Form");
+                    tables.Add("PV_Result");
+                }
+
+                this.DialogResult = DialogResult.OK;
+
+                this.Close();
+            }
+            
+        }
+
+    }
+
+}
+
