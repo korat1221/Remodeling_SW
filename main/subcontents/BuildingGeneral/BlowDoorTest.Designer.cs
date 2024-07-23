82c1a3a55ea3d3f75fe4337b5304716e30543a80
korat1221 <108967572+korat1221@users.noreply.github.com>
2024-07-23 11:53:24 +0900

    blowdoortest 폼 추가

---
+++ main/subcontents/BuildingGeneral/BlowDoorTest.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace main.subcontents.BuildingGeneral;
+
+partial class BlowDoorTest
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+        DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+        DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+        Save_button = new Button();
+        Blow_dataGridView = new DataGridView();
+        Add_button = new Button();
+        Delete_button = new Button();
+        label4 = new Label();
+        n50_textBox = new TextBox();
+        n50_label1 = new Label();
+        n50_label2 = new Label();
+        ((System.ComponentModel.ISupportInitialize)Blow_dataGridView).BeginInit();
+        SuspendLayout();
+        // 
+        // Save_button
+        // 
+        Save_button.BackColor = SystemColors.ButtonHighlight;
+        Save_button.ForeColor = Color.Black;
+        Save_button.Location = new Point(232, 207);
+        Save_button.Name = "Save_button";
+        Save_button.Size = new Size(135, 25);
+        Save_button.TabIndex = 20;
+        Save_button.Text = "SAVE";
+        Save_button.UseVisualStyleBackColor = true;
+        Save_button.Click += Save_button_Click;
+        // 
+        // Blow_dataGridView
+        // 
+        Blow_dataGridView.AllowUserToAddRows = false;
+        Blow_dataGridView.AllowUserToDeleteRows = false;
+        Blow_dataGridView.AllowUserToResizeColumns = false;
+        Blow_dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        Blow_dataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+        Blow_dataGridView.BackgroundColor = Color.White;
+        Blow_dataGridView.BorderStyle = BorderStyle.None;
+        Blow_dataGridView.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+        Blow_dataGridView.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+        dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+        dataGridViewCellStyle1.BackColor = SystemColors.GradientActiveCaption;
+        dataGridViewCellStyle1.Font = new Font("맑은 고딕", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+        dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+        dataGridViewCellStyle1.SelectionBackColor = SystemColors.GradientActiveCaption;
+        dataGridViewCellStyle1.SelectionForeColor = Color.Black;
+        dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+        Blow_dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+        Blow_dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+        Blow_dataGridView.Location = new Point(0, 32);
+        Blow_dataGridView.Name = "Blow_dataGridView";
+        dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+        dataGridViewCellStyle2.BackColor = SystemColors.Control;
+        dataGridViewCellStyle2.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+        dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+        dataGridViewCellStyle2.SelectionBackColor = SystemColors.Control;
+        dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+        dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+        Blow_dataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+        Blow_dataGridView.RowHeadersVisible = false;
+        Blow_dataGridView.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+        dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+        dataGridViewCellStyle3.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+        dataGridViewCellStyle3.ForeColor = Color.Black;
+        dataGridViewCellStyle3.SelectionBackColor = SystemColors.GradientInactiveCaption;
+        dataGridViewCellStyle3.SelectionForeColor = Color.Black;
+        Blow_dataGridView.RowsDefaultCellStyle = dataGridViewCellStyle3;
+        Blow_dataGridView.RowTemplate.Height = 25;
+        Blow_dataGridView.Size = new Size(391, 141);
+        Blow_dataGridView.TabIndex = 19;
+        Blow_dataGridView.CellContentClick += Blow_dataGridView_CellContentClick;
+        Blow_dataGridView.CellValueChanged += Blow_dataGridView_CellValueChanged;
+        // 
+        // Add_button
+        // 
+        Add_button.BackColor = SystemColors.ControlLight;
+        Add_button.FlatAppearance.BorderColor = SystemColors.ActiveCaption;
+        Add_button.FlatStyle = FlatStyle.System;
+        Add_button.Font = new Font("Microsoft Sans Serif", 11.9999981F, FontStyle.Bold, GraphicsUnit.Point);
+        Add_button.Location = new Point(327, 6);
+        Add_button.Margin = new Padding(0);
+        Add_button.Name = "Add_button";
+        Add_button.Size = new Size(23, 23);
+        Add_button.TabIndex = 172;
+        Add_button.Text = "+";
+        Add_button.UseVisualStyleBackColor = false;
+        Add_button.Click += Add_button_Click;
+        // 
+        // Delete_button
+        // 
+        Delete_button.BackColor = SystemColors.ControlLight;
+        Delete_button.FlatAppearance.BorderColor = SystemColors.ActiveCaption;
+        Delete_button.FlatStyle = FlatStyle.System;
+        Delete_button.Font = new Font("Microsoft Sans Serif", 11.9999981F, FontStyle.Bold, GraphicsUnit.Point);
+        Delete_button.Location = new Point(354, 6);
+        Delete_button.Margin = new Padding(0);
+        Delete_button.Name = "Delete_button";
+        Delete_button.Size = new Size(23, 23);
+        Delete_button.TabIndex = 173;
+        Delete_button.Text = "-";
+        Delete_button.UseVisualStyleBackColor = false;
+        Delete_button.Click += Delete_button_Click;
+        // 
+        // label4
+        // 
+        label4.AutoSize = true;
+        label4.Font = new Font("맑은 고딕", 9F, FontStyle.Bold, GraphicsUnit.Point);
+        label4.Location = new Point(12, 10);
+        label4.Name = "label4";
+        label4.Size = new Size(99, 15);
+        label4.TabIndex = 174;
+        label4.Text = "기밀 테스트 결과";
+        // 
+        // n50_textBox
+        // 
+        n50_textBox.BackColor = Color.White;
+        n50_textBox.BorderStyle = BorderStyle.None;
+        n50_textBox.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+        n50_textBox.ForeColor = SystemColors.ControlText;
+        n50_textBox.Location = new Point(232, 186);
+        n50_textBox.Name = "n50_textBox";
+        n50_textBox.Size = new Size(120, 15);
+        n50_textBox.TabIndex = 176;
+        n50_textBox.TextAlign = HorizontalAlignment.Center;
+        // 
+        // n50_label1
+        // 
+        n50_label1.AutoSize = true;
+        n50_label1.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+        n50_label1.ForeColor = SystemColors.ControlText;
+        n50_label1.Location = new Point(175, 187);
+        n50_label1.Name = "n50_label1";
+        n50_label1.Size = new Size(56, 15);
+        n50_label1.TabIndex = 175;
+        n50_label1.Text = "평균 n50";
+        // 
+        // n50_label2
+        // 
+        n50_label2.AutoSize = true;
+        n50_label2.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+        n50_label2.ForeColor = SystemColors.ControlText;
+        n50_label2.Location = new Point(352, 185);
+        n50_label2.Name = "n50_label2";
+        n50_label2.Size = new Size(25, 16);
+        n50_label2.TabIndex = 177;
+        n50_label2.Text = "1/h";
+        // 
+        // BlowDoorTest
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        BackColor = Color.White;
+        ClientSize = new Size(393, 244);
+        Controls.Add(n50_label2);
+        Controls.Add(n50_textBox);
+        Controls.Add(n50_label1);
+        Controls.Add(label4);
+        Controls.Add(Delete_button);
+        Controls.Add(Add_button);
+        Controls.Add(Blow_dataGridView);
+        Controls.Add(Save_button);
+        FormBorderStyle = FormBorderStyle.FixedToolWindow;
+        Name = "BlowDoorTest";
+        Text = "BlowDoor Test";
+        ((System.ComponentModel.ISupportInitialize)Blow_dataGridView).EndInit();
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+    private Button Save_button;
+    private DataGridView Blow_dataGridView;
+    private TextBox textBox2;
+    private TextBox d_ins_textBox;
+    private Button Add_button;
+    private Button Delete_button;
+    private Label label4;
+    private TextBox n50_textBox;
+    private Label n50_label1;
+    private Label n50_label2;
+}
---
+++ main/subcontents/BuildingGeneral/BlowDoorTest.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace main.subcontents.BuildingGeneral;
+
+public partial class BlowDoorTest : Form
+{
+    int SelectRow;
+    public double n50;
+    public BlowDoorTest()
+    {
+        InitializeComponent();
+        create_table();
+        Load_Value();
+    }
+    private void create_table()
+    {
+        new StackedHeaderDecorator(Blow_dataGridView, DataGridViewAutoSizeColumnsMode.Fill, datagridviewDesign);
+        DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn();
+        Blow_dataGridView.Columns.Clear();
+        checkBoxColumn.HeaderText = "선택";
+        checkBoxColumn.Name = "check";
+        Blow_dataGridView.Columns.Add(checkBoxColumn);
+
+        Blow_dataGridView.Columns.Add("A1", "번호");
+        Blow_dataGridView.Columns.Add("A2", "측정 위치");
+        Blow_dataGridView.Columns.Add("A3", "AirFlow @50.[m³/h]");
+        Blow_dataGridView.Columns.Add("A4", "ACH @50.[1/h]");
+        Blow_dataGridView.Columns.Add("A5", "Volume.[m³]");
+        Blow_dataGridView.Columns[0].Width = 30;
+        Blow_dataGridView.Columns[1].Width = 30;
+        Blow_dataGridView.Columns[2].Width = 80;
+        Blow_dataGridView.Columns[4].Width = 80;
+        Blow_dataGridView.Columns[5].Width = 80;
+    }
+
+    private Boolean datagridviewDesign(DataGridViewCell cell, int column, int row)
+    {
+        if (column == 2)
+        {
+            if (cell.Value == null || cell.Value.ToString() == "")
+            {
+                cell.Style.BackColor = SystemColors.Info;
+            }
+            else
+            {
+                cell.Style.BackColor = Color.FromArgb(255, 255, 255);
+            }
+        }
+        if (column == 3 )
+        {
+            if(cell.Value == null || cell.Value.ToString() == "")
+            {
+                cell.Style.BackColor = SystemColors.Info;
+            }
+            else
+            {
+                cell.Style.BackColor = Color.FromArgb(255, 255, 255);
+            }           
+        }
+        if (column == 4 )
+        {
+            if (cell.Value == null || cell.Value.ToString() == "")
+            {
+                cell.Style.BackColor = SystemColors.Info;
+            }
+            else
+            {
+                cell.Style.BackColor = Color.FromArgb(255, 255, 255);
+            }
+        }
+        if (column == 5 )
+        {
+            cell.Style.BackColor = Color.FromArgb(255, 255, 255);
+        }
+        return true;
+    }
+
+    private void Add_button_Click(object sender, EventArgs e)
+    {
+        int nRow = Blow_dataGridView.Rows.Add();
+        Load_Num();
+    }
+
+    private void Load_Num()
+    {
+        for (int k = 0; k < Blow_dataGridView.RowCount; k++)
+        {
+            Blow_dataGridView.Rows[k].Cells[1].Value = (k + 1).ToString();
+        }
+    }
+    private void Delete_button_Click(object sender, EventArgs e)
+    {
+        Blow_dataGridView.Rows.Remove(Blow_dataGridView.Rows[SelectRow]);
+        Load_Num();
+    }
+
+    private void Blow_dataGridView_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+    {
+        if(e.ColumnIndex == 3 || e.ColumnIndex == 4)
+        {
+            if((Blow_dataGridView.Rows[e.RowIndex].Cells[3].Value !=null && Blow_dataGridView.Rows[e.RowIndex].Cells[3].Value.ToString() != "") && (Blow_dataGridView.Rows[e.RowIndex].Cells[4].Value != null && Blow_dataGridView.Rows[e.RowIndex].Cells[4].Value.ToString() != "" && Blow_dataGridView.Rows[e.RowIndex].Cells[4].Value.ToString() != "0"))
+            {
+                Blow_dataGridView.Rows[e.RowIndex].Cells[5].Value = (Convert.ToDouble(Blow_dataGridView.Rows[e.RowIndex].Cells[3].Value.ToString()) / Convert.ToDouble(Blow_dataGridView.Rows[e.RowIndex].Cells[4].Value.ToString())).ToString("0.0");
+                Cal_n50_tot();
+            }
+        }
+    }
+
+    private Boolean Check_Void()
+    {
+        Boolean check = false;
+        for(int a =0; a< Blow_dataGridView.Rows.Count; a++)
+        {
+            if (Blow_dataGridView.Rows[a].Cells[5].Value == null)
+            {
+               check = false;
+                goto stop;
+            }
+            else
+            {
+                check = true;
+            }
+        }
+        stop:
+        return check;
+    }
+    private void Cal_n50_tot()
+    {
+        double CMH_tot = 0; double volume_tot = 0;
+        if(Check_Void() ==true)
+        {
+            for (int a = 0; a < Blow_dataGridView.Rows.Count; a++)
+            {
+                CMH_tot += Convert.ToDouble(Blow_dataGridView.Rows[a].Cells[3].Value.ToString());
+                volume_tot += Convert.ToDouble(Blow_dataGridView.Rows[a].Cells[5].Value.ToString());
+                n50 = CMH_tot / volume_tot;
+                n50_textBox.Text = n50.ToString("0.00") ;
+            }
+        }       
+    }
+    private void Save_button_Click(object sender, EventArgs e)
+    {
+        Boolean check = false;
+        for (int a = 0; a < Blow_dataGridView.Rows.Count; a++)
+        {
+            if (Blow_dataGridView.Rows[a].Cells[2].Value != null && Blow_dataGridView.Rows[a].Cells[5].Value != null && Blow_dataGridView.Rows[a].Cells[5].Value.ToString() != "")
+            {
+                check = true;
+            }
+        }
+
+        if (check ==true)
+        {
+            Program.DB.deleteTable(DB.type.ProjDB, "BlowDoorTest");
+            Program.DB.initTable(DB.type.ProjDB, "BlowDoorTest");
+            for (int a = 0; a < Blow_dataGridView.Rows.Count; a++)
+            {
+                Program.DB.setValue(DB.type.ProjDB, "BlowDoorTest", "번호,측정위치,CMH,ACH,Volume",
+                "'" + Blow_dataGridView.Rows[a].Cells[1].Value.ToString() + "','"+
+                Blow_dataGridView.Rows[a].Cells[2].Value.ToString() + "','" +
+                Blow_dataGridView.Rows[a].Cells[3].Value.ToString() + "','" +
+                Blow_dataGridView.Rows[a].Cells[4].Value.ToString() + "','" +
+                Blow_dataGridView.Rows[a].Cells[5].Value.ToString() 
+                + "'", "번호");
+            }
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+        else
+        {
+            MessageBox.Show("모든 값을 입력해주세요.");
+        }
+    }
+    private void Load_Value()
+    {
+        string[][] Value = Program.DB.getValue(DB.type.ProjDB, "BlowDoorTest", "번호,측정위치,CMH,ACH,Volume", "");
+        if(Value.Length > 0)
+        {
+            for(int a=0; a< Value.Length; a++)
+            {
+                int nRow = Blow_dataGridView.Rows.Add();
+                Blow_dataGridView.Rows[a].Cells[1].Value = Value[a][0];
+                Blow_dataGridView.Rows[a].Cells[2].Value = Value[a][1];
+                Blow_dataGridView.Rows[a].Cells[3].Value = Value[a][2];
+                Blow_dataGridView.Rows[a].Cells[4].Value = Value[a][3];
+            }
+        }
+    }
+       
+
+    private void Blow_dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+    {
+        if (e.RowIndex >= 0)
+        {
+            Blow_dataGridView.CommitEdit(DataGridViewDataErrorContexts.Commit);
+            SelectRow = e.RowIndex;
+        }
+    }
+
+}
