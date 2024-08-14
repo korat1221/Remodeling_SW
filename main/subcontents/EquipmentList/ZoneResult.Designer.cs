23402891c7ed27aa7366033ae162432cdb0a8bdc
korat1221 <108967572+korat1221@users.noreply.github.com>
2024-08-14 10:36:20 +0900

    존 요구량, 부하 검토 폼 추가

---
+++ main/subcontents/EquipmentList/ZoneResult.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace main.subcontents
+{
+    partial class ZoneResult
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle7 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle8 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle9 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle10 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle11 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle12 = new DataGridViewCellStyle();
+            panel1 = new Panel();
+            ZoneResult_dataGridView = new DataGridView();
+            QmaxList_panel = new DataGridView();
+            Icon_pictureBox = new PictureBox();
+            label35 = new Label();
+            Qhb_textBox = new TextBox();
+            label26 = new Label();
+            label34 = new Label();
+            Qcb_textBox = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            Qcmax_textBox = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            Qhmax_textBox = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            Qhmax_Copy_button = new Button();
+            Qcmax_Copy_button = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)ZoneResult_dataGridView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)QmaxList_panel).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Icon_pictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(ZoneResult_dataGridView);
+            panel1.Controls.Add(QmaxList_panel);
+            panel1.Location = new Point(24, 80);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(811, 452);
+            panel1.TabIndex = 2;
+            // 
+            // ZoneResult_dataGridView
+            // 
+            ZoneResult_dataGridView.AllowUserToAddRows = false;
+            ZoneResult_dataGridView.AllowUserToDeleteRows = false;
+            ZoneResult_dataGridView.AllowUserToResizeColumns = false;
+            ZoneResult_dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            ZoneResult_dataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            ZoneResult_dataGridView.BackgroundColor = Color.White;
+            ZoneResult_dataGridView.BorderStyle = BorderStyle.None;
+            ZoneResult_dataGridView.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            ZoneResult_dataGridView.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle7.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle7.BackColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle7.Font = new Font("맑은 고딕", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle7.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle7.SelectionBackColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle7.SelectionForeColor = Color.Black;
+            dataGridViewCellStyle7.WrapMode = DataGridViewTriState.True;
+            ZoneResult_dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            ZoneResult_dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            ZoneResult_dataGridView.Dock = DockStyle.Fill;
+            ZoneResult_dataGridView.Location = new Point(0, 0);
+            ZoneResult_dataGridView.Name = "ZoneResult_dataGridView";
+            dataGridViewCellStyle8.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle8.BackColor = SystemColors.Control;
+            dataGridViewCellStyle8.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle8.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle8.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle8.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = DataGridViewTriState.True;
+            ZoneResult_dataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle8;
+            ZoneResult_dataGridView.RowHeadersVisible = false;
+            ZoneResult_dataGridView.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle9.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle9.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle9.ForeColor = Color.Black;
+            dataGridViewCellStyle9.SelectionBackColor = SystemColors.GradientInactiveCaption;
+            dataGridViewCellStyle9.SelectionForeColor = Color.Black;
+            ZoneResult_dataGridView.RowsDefaultCellStyle = dataGridViewCellStyle9;
+            ZoneResult_dataGridView.RowTemplate.Height = 25;
+            ZoneResult_dataGridView.Size = new Size(811, 452);
+            ZoneResult_dataGridView.TabIndex = 20;
+            ZoneResult_dataGridView.CellContentClick += ZoneResult_dataGridView_CellContentClick;
+            // 
+            // QmaxList_panel
+            // 
+            QmaxList_panel.AllowUserToAddRows = false;
+            QmaxList_panel.AllowUserToDeleteRows = false;
+            QmaxList_panel.AllowUserToResizeColumns = false;
+            QmaxList_panel.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            QmaxList_panel.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            QmaxList_panel.BackgroundColor = SystemColors.ActiveCaption;
+            QmaxList_panel.BorderStyle = BorderStyle.None;
+            QmaxList_panel.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            QmaxList_panel.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle10.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle10.BackColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle10.Font = new Font("맑은 고딕", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle10.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle10.SelectionBackColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle10.SelectionForeColor = Color.Black;
+            dataGridViewCellStyle10.WrapMode = DataGridViewTriState.True;
+            QmaxList_panel.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle10;
+            QmaxList_panel.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            QmaxList_panel.Dock = DockStyle.Fill;
+            QmaxList_panel.Location = new Point(0, 0);
+            QmaxList_panel.Name = "QmaxList_panel";
+            dataGridViewCellStyle11.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle11.BackColor = SystemColors.Control;
+            dataGridViewCellStyle11.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle11.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle11.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle11.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle11.WrapMode = DataGridViewTriState.True;
+            QmaxList_panel.RowHeadersDefaultCellStyle = dataGridViewCellStyle11;
+            QmaxList_panel.RowHeadersVisible = false;
+            QmaxList_panel.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle12.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle12.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle12.ForeColor = Color.Black;
+            dataGridViewCellStyle12.SelectionBackColor = SystemColors.GradientInactiveCaption;
+            dataGridViewCellStyle12.SelectionForeColor = Color.Black;
+            QmaxList_panel.RowsDefaultCellStyle = dataGridViewCellStyle12;
+            QmaxList_panel.RowTemplate.Height = 25;
+            QmaxList_panel.Size = new Size(811, 452);
+            QmaxList_panel.TabIndex = 100;
+            // 
+            // Icon_pictureBox
+            // 
+            Icon_pictureBox.Location = new Point(27, 22);
+            Icon_pictureBox.Name = "Icon_pictureBox";
+            Icon_pictureBox.Size = new Size(52, 50);
+            Icon_pictureBox.TabIndex = 3;
+            Icon_pictureBox.TabStop = false;
+            // 
+            // label35
+            // 
+            label35.AutoSize = true;
+            label35.Font = new Font("맑은 고딕", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label35.Location = new Point(114, 11);
+            label35.Name = "label35";
+            label35.Size = new Size(83, 17);
+            label35.TabIndex = 169;
+            label35.Text = "선택 존 합계";
+            label35.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Qhb_textBox
+            // 
+            Qhb_textBox.BackColor = Color.White;
+            Qhb_textBox.BorderStyle = BorderStyle.None;
+            Qhb_textBox.Enabled = false;
+            Qhb_textBox.Font = new Font("맑은 고딕", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            Qhb_textBox.Location = new Point(255, 33);
+            Qhb_textBox.Name = "Qhb_textBox";
+            Qhb_textBox.Size = new Size(70, 16);
+            Qhb_textBox.TabIndex = 168;
+            Qhb_textBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label26
+            // 
+            label26.AutoSize = true;
+            label26.Location = new Point(331, 34);
+            label26.Name = "label26";
+            label26.Size = new Size(42, 15);
+            label26.TabIndex = 167;
+            label26.Text = "kWh/a";
+            // 
+            // label34
+            // 
+            label34.AutoSize = true;
+            label34.Location = new Point(114, 34);
+            label34.Name = "label34";
+            label34.Size = new Size(135, 15);
+            label34.TabIndex = 166;
+            label34.Text = "연간 난방 에너지요구량";
+            // 
+            // Qcb_textBox
+            // 
+            Qcb_textBox.BackColor = Color.White;
+            Qcb_textBox.BorderStyle = BorderStyle.None;
+            Qcb_textBox.Enabled = false;
+            Qcb_textBox.Font = new Font("맑은 고딕", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            Qcb_textBox.Location = new Point(255, 58);
+            Qcb_textBox.Name = "Qcb_textBox";
+            Qcb_textBox.Size = new Size(70, 16);
+            Qcb_textBox.TabIndex = 172;
+            Qcb_textBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(331, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 171;
+            label1.Text = "kWh/a";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(114, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(135, 15);
+            label2.TabIndex = 170;
+            label2.Text = "연간 냉방 에너지요구량";
+            // 
+            // Qcmax_textBox
+            // 
+            Qcmax_textBox.BackColor = Color.White;
+            Qcmax_textBox.BorderStyle = BorderStyle.None;
+            Qcmax_textBox.Enabled = false;
+            Qcmax_textBox.Font = new Font("맑은 고딕", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            Qcmax_textBox.Location = new Point(472, 58);
+            Qcmax_textBox.Name = "Qcmax_textBox";
+            Qcmax_textBox.Size = new Size(70, 16);
+            Qcmax_textBox.TabIndex = 178;
+            Qcmax_textBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(548, 59);
+            label3.Name = "label3";
+            label3.Size = new Size(24, 15);
+            label3.TabIndex = 177;
+            label3.Text = "kW";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(379, 59);
+            label4.Name = "label4";
+            label4.Size = new Size(87, 15);
+            label4.TabIndex = 176;
+            label4.Text = "최대 냉방 부하";
+            // 
+            // Qhmax_textBox
+            // 
+            Qhmax_textBox.BackColor = Color.White;
+            Qhmax_textBox.BorderStyle = BorderStyle.None;
+            Qhmax_textBox.Enabled = false;
+            Qhmax_textBox.Font = new Font("맑은 고딕", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            Qhmax_textBox.Location = new Point(472, 33);
+            Qhmax_textBox.Name = "Qhmax_textBox";
+            Qhmax_textBox.Size = new Size(70, 16);
+            Qhmax_textBox.TabIndex = 175;
+            Qhmax_textBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(548, 34);
+            label5.Name = "label5";
+            label5.Size = new Size(24, 15);
+            label5.TabIndex = 174;
+            label5.Text = "kW";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(379, 34);
+            label6.Name = "label6";
+            label6.Size = new Size(87, 15);
+            label6.TabIndex = 173;
+            label6.Text = "최대 난방 부하";
+            // 
+            // Qhmax_Copy_button
+            // 
+            Qhmax_Copy_button.BackColor = SystemColors.ControlLight;
+            Qhmax_Copy_button.FlatAppearance.BorderColor = SystemColors.ActiveCaption;
+            Qhmax_Copy_button.FlatStyle = FlatStyle.System;
+            Qhmax_Copy_button.Font = new Font("Microsoft Sans Serif", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            Qhmax_Copy_button.Location = new Point(575, 30);
+            Qhmax_Copy_button.Margin = new Padding(0);
+            Qhmax_Copy_button.Name = "Qhmax_Copy_button";
+            Qhmax_Copy_button.Size = new Size(47, 23);
+            Qhmax_Copy_button.TabIndex = 179;
+            Qhmax_Copy_button.Text = "Copy";
+            Qhmax_Copy_button.UseVisualStyleBackColor = false;
+            Qhmax_Copy_button.Click += Qhmax_Copy_button_Click;
+            // 
+            // Qcmax_Copy_button
+            // 
+            Qcmax_Copy_button.BackColor = SystemColors.ControlLight;
+            Qcmax_Copy_button.FlatAppearance.BorderColor = SystemColors.ActiveCaption;
+            Qcmax_Copy_button.FlatStyle = FlatStyle.System;
+            Qcmax_Copy_button.Font = new Font("Microsoft Sans Serif", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            Qcmax_Copy_button.Location = new Point(575, 55);
+            Qcmax_Copy_button.Margin = new Padding(0);
+            Qcmax_Copy_button.Name = "Qcmax_Copy_button";
+            Qcmax_Copy_button.Size = new Size(47, 23);
+            Qcmax_Copy_button.TabIndex = 180;
+            Qcmax_Copy_button.Text = "Copy";
+            Qcmax_Copy_button.UseVisualStyleBackColor = false;
+            Qcmax_Copy_button.Click += Qcmax_Copy_button_Click;
+            // 
+            // ZoneResult
+            // 
+            AllowDrop = true;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(856, 559);
+            Controls.Add(Qcmax_Copy_button);
+            Controls.Add(Qhmax_Copy_button);
+            Controls.Add(Qcmax_textBox);
+            Controls.Add(label3);
+            Controls.Add(label4);
+            Controls.Add(Qhmax_textBox);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(Qcb_textBox);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(label35);
+            Controls.Add(Qhb_textBox);
+            Controls.Add(label26);
+            Controls.Add(label34);
+            Controls.Add(Icon_pictureBox);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "ZoneResult";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "존 계산 결과 확인";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)ZoneResult_dataGridView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)QmaxList_panel).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Icon_pictureBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button Save_Button;
+        private Panel panel1;
+        private DataGridView QmaxList_panel;
+        private PictureBox Icon_pictureBox;
+        private DataGridView ZoneResult_dataGridView;
+        private DataGridView Zone_dataGridView;
+        private Label label1;
+        private Label ZoneCount;
+        private Label label35;
+        private TextBox UserDB_UfA_textBox;
+        private Label label26;
+        private Label label34;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label5;
+        private Label label6;
+        private Button AirHP_Copy_button;
+        private Button button1;
+        private TextBox Qhb_textBox;
+        private TextBox Qcb_textBox;
+        private TextBox Qcmax_textBox;
+        private TextBox Qhmax_textBox;
+        private Button Qhmax_Copy_button;
+        private Button Qcmax_Copy_button;
+    }
+}
---
+++ main/subcontents/EquipmentList/ZoneResult.cs
@@ -0,0 +1,171 @@
+﻿using main.contents;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common; 
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.ComponentModel.Design.ObjectSelectorEditor;
+
+namespace main.subcontents
+{
+    public partial class ZoneResult : Form
+    {
+        public ZoneResult()
+        {
+            InitializeComponent();
+            Icon_pictureBox.Load(Program.gPath + "images/1sticon/4.Zone_on3.png");
+            Icon_pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            load_table_DB();
+        }
+
+        void load_table_DB()
+        {
+            new StackedHeaderDecorator(ZoneResult_dataGridView, DataGridViewAutoSizeColumnsMode.Fill, datagridviewDesign);
+
+            DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn();
+            ZoneResult_dataGridView.Columns.Clear();
+            checkBoxColumn.HeaderText = "선택";
+            checkBoxColumn.Name = "check";
+            ZoneResult_dataGridView.Columns.Add(checkBoxColumn);
+
+            ZoneResult_dataGridView.Columns.Add("A1", "번호");
+            ZoneResult_dataGridView.Columns.Add("A2", "층");
+            ZoneResult_dataGridView.Columns.Add("A3", "존명칭");
+            ZoneResult_dataGridView.Columns.Add("A4", "용도프로필");
+            ZoneResult_dataGridView.Columns.Add("A5", "면적.[m²]");
+            ZoneResult_dataGridView.Columns.Add("A6", "연간 요구량.난방.[kWh/a]");
+            ZoneResult_dataGridView.Columns.Add("A7", "연간 요구량.냉방.[kWh/a]");
+            ZoneResult_dataGridView.Columns.Add("A8", "최대부하.난방.[kW]");
+            ZoneResult_dataGridView.Columns.Add("A9", "최대부하.냉방.[kW]");
+
+            ZoneResult_dataGridView.Columns[0].Width = 40;
+            ZoneResult_dataGridView.Columns[2].Width = 60;
+
+            string[][] Value = Program.DB.querySQL(DB.type.ProjDB, "Select 존번호,존이름,용도프로필,순바닥면적 from ZoneGeneral_Form where Not 냉난방유무 ='비냉난방'  Order by  존번호");
+            if (Value.Length > 0)
+            {
+                for (int n = 0; n < Value.Length; n++)
+                {
+                    string[][] 층 = Program.DB.getValue(DB.type.ProjDB, "ZoneEnvelope_3D", "층", "존 ='" + Value[n][0] + "'");
+
+
+                    int nRow = ZoneResult_dataGridView.Rows.Add();
+                    ZoneResult_dataGridView.Rows[nRow].Cells[1].Value = Value[n][0];
+                    if (층.Length > 0)
+                    {
+                        ZoneResult_dataGridView.Rows[nRow].Cells[2].Value = 층[0][0];
+                    }
+                    ZoneResult_dataGridView.Rows[nRow].Cells[3].Value = Value[n][1];
+                    ZoneResult_dataGridView.Rows[nRow].Cells[4].Value = Value[n][2];
+                    ZoneResult_dataGridView.Rows[nRow].Cells[5].Value = string.Format("{0:F1}", Convert.ToDouble(Value[n][3]));
+
+                    string[][] 난방부하 = Program.DB.getValue_SameCheck(DB.type.ProjDB, "Zone_HCneed_Result", "Qb_a,Q_max", "번호 ='" + Value[n][0] + "' AND 난방_냉방 = '난방'");
+                    if (난방부하.Length > 0)
+                    {
+                        ZoneResult_dataGridView.Rows[nRow].Cells[6].Value = Convert.ToDouble(난방부하[0][0]).ToString("0");
+                        ZoneResult_dataGridView.Rows[nRow].Cells[8].Value = (Convert.ToDouble(난방부하[0][1]) / 1000).ToString("0.00");
+                    }
+                    string[][] 냉방부하 = Program.DB.getValue_SameCheck(DB.type.ProjDB, "Zone_HCneed_Result", "Qb_a,Q_max", "번호 ='" + Value[n][0] + "' AND 난방_냉방 = '냉방'");
+                    if (냉방부하.Length > 0)
+                    {
+                        ZoneResult_dataGridView.Rows[nRow].Cells[7].Value = Convert.ToDouble(냉방부하[0][0]).ToString("0");
+                        ZoneResult_dataGridView.Rows[nRow].Cells[9].Value = (Convert.ToDouble(냉방부하[0][1]) / 1000).ToString("0.00");
+                    }
+                }
+            }
+        }
+
+        private Boolean datagridviewDesign(DataGridViewCell cell, int column, int row)
+        {
+            if (row % 2 == 1)
+            {
+                cell.Style.BackColor = SystemColors.InactiveBorder;
+                cell.Style.ForeColor = Color.Black;
+                cell.Style.SelectionBackColor = SystemColors.InactiveBorder;
+                cell.Style.SelectionForeColor = Color.Black;
+                return true;
+            }
+            else
+            {
+                cell.Style.BackColor = Color.FromArgb(255, 255, 255);
+                cell.Style.ForeColor = Color.Black;
+                cell.Style.SelectionBackColor = Color.FromArgb(255, 255, 255);
+                cell.Style.SelectionForeColor = Color.Black;
+                return true;
+            }
+        }
+
+        private void SelectCalc()
+        {
+            double Qhb = 0; double Qcb = 0; double Qhmax = 0; double Qcmax = 0;
+            foreach (DataGridViewRow row in ZoneResult_dataGridView.Rows)
+            {
+                if (Convert.ToBoolean(row.Cells["check"].Value))
+                {
+                    row.DefaultCellStyle.SelectionBackColor = SystemColors.GradientInactiveCaption;
+                    if (row.Cells[6].Value != null)
+                    { Qhb += Convert.ToDouble(row.Cells[6].Value.ToString()); }
+                    if (row.Cells[7].Value != null)
+                    { Qcb += Convert.ToDouble(row.Cells[7].Value.ToString()); }
+                    if (row.Cells[8].Value != null)
+                    { Qhmax += Convert.ToDouble(row.Cells[8].Value.ToString()); }
+                    if (row.Cells[9].Value != null)
+                    { Qcmax += Convert.ToDouble(row.Cells[9].Value.ToString()); }
+                }
+            }
+            if(Qhb > 0) 
+            {  
+                Qhb_textBox.Text = Qhb.ToString();
+                controls.ThousandsSeparator textbox = new controls.ThousandsSeparator(Qhb_textBox, true, 0);
+            }
+            if (Qcb > 0)
+            {
+                Qcb_textBox.Text = Qcb.ToString();
+                controls.ThousandsSeparator textbox = new controls.ThousandsSeparator(Qcb_textBox, true, 0);
+            }
+            if (Qhmax > 0)
+            {
+                Qhmax_textBox.Text = Qhmax.ToString();
+                controls.ThousandsSeparator textbox = new controls.ThousandsSeparator(Qhmax_textBox, true, 2);
+            }
+            if (Qcmax > 0)
+            {
+                Qcmax_textBox.Text = Qcmax.ToString();
+                controls.ThousandsSeparator textbox = new controls.ThousandsSeparator(Qcmax_textBox, true, 2);
+            }
+        }
+
+        private void Qhmax_Copy_button_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Qhmax_textBox.Text.ToString());
+            MessageBox.Show("최대 난방 부하 값이 복사되었습니다.");
+        }
+
+        private void Qcmax_Copy_button_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Qcmax_textBox.Text.ToString());
+            MessageBox.Show("최대 냉방 부하 값이 복사되었습니다.");
+        }
+
+        private void ZoneResult_dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >-1)
+            {
+                SelectCalc();
+            }
+        }
+    }
+}
