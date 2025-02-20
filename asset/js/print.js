ecacb8c1ce5db2fb1a984093e066b76f72c5e533
korat1221 <108967572+korat1221@users.noreply.github.com>
2025-02-20 16:15:37 +0900

    레포트 폴더 위치 변경

--- asset/js/print.js
+++ asset/js/print.js
@@ -1,24 +1,69 @@
-
 window.addEventListener("message", async (event) => {
-    let o = event.data;
-    if (o.print) {
-        window.print();
+  let o = event.data;
+  if (o.print) {
+    window.print();
+  } else if (o.init) {
+    let i = -1,
+      j;
+
+    $("div").remove();
+
+    while (++i < o.pages.length) {
+      if (i > 0) {
+        $("body").append("<div style='page-break-before:always'></div>");
+      }
+      $("body").append($("<div>").load("/print/" + o.pages[i]));
     }
-    else if (o.init) {
-        let i = -1;
 
-        while(++i < o.zpages.length) {
-            let zpage = o.zpages[i];
+    setTimeout(() => {
+      let i = -1;
 
-            $( 'html' ).append($('<div>').load('zpage.html'));
+      while (++i < o.items.length) {
+        let item = o.items[i];
+
+        j = -1;
+        while (++j < item.data.length) {
+          let el = item.data[j]; 
+
+            // 'cname'이 'projectnum'인 값을 찾아 projectNum 변수에 저장
+            if (item.cname === 'projectnum') {
+              projectNum = el.val;
+            }         
+          $("." + item.cname)
+            .eq(el.idx)
+            .html(el.val); 
+        } 
+
+        let projectNumValue = o.items.find(item => item.cname === "projectnum");
+        let zoneNumValue = o.items.find(item => item.cname === "zonenum");
+        let coolingNumValue = o.items.find(item => item.cname === "coolingnum");
+        $(".buildingImage").each((idx,al) => {
+          let projectNum = projectNumValue.data[idx].val; 
+          al.setAttribute("src", "img/" + projectNum + "/Building.png");
+        });
+        $(".zoneImage").each((idx,al) => {
+          let projectNum = projectNumValue.data[idx].val; 
+          let zoneNum = zoneNumValue.data[idx].val; 
+          al.setAttribute("src", "img/" + projectNum + "/"+ zoneNum + ".png"); 
+        });
+        $(".coolingImage").each((idx,al) => {
+          let projectNum = projectNumValue.data[idx].val; 
+          let coolingNum = coolingNumValue.data[idx].val; 
+          al.setAttribute("src", "img/" + projectNum + "/"+ coolingNum + ".png"); 
+        });
+      }
+ 
+      setTimeout(() => {
+        const ifrms = document.querySelectorAll('.ifrm-chart1');
+
+        i = -1;
+        while(++i < ifrms.length) {
+          o.chart[i].chart = true;
+          ifrms[i].contentWindow.postMessage(o.chart[i], "*");
         }
-    }
+      }, 1000);
+    }, 100);
+  }
 });
 
-$(function(){
-    let i = -1;
-
-    while(++i < 4) {
-        $( 'body' ).append($('<div>').load('zpage.html'));
-    }
-});
+$(function () {});
