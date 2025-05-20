54c2b8513cbf03e1027be503c974c662f33b2f93
korat1221 <108967572+korat1221@users.noreply.github.com>
2025-05-20 15:13:25 +0900

    필요없는 이미지 삭제

--- main/MainContents.Designer.cs
+++ main/MainContents.Designer.cs
@@ -31,10 +31,8 @@
             components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainContents));
             imageList1 = new ImageList(components);
-            pictureBox7 = new PictureBox();
             webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
             timer1 = new System.Windows.Forms.Timer(components);
-            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
             ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
             SuspendLayout();
             // 
@@ -51,16 +49,6 @@
             imageList1.Images.SetKeyName(5, "2_6.png");
             imageList1.Images.SetKeyName(6, "2_7.png");
             // 
-            // pictureBox7
-            // 
-            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
-            pictureBox7.Location = new Point(448, 621);
-            pictureBox7.Name = "pictureBox7";
-            pictureBox7.Size = new Size(30, 30);
-            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox7.TabIndex = 8;
-            pictureBox7.TabStop = false;
-            // 
             // webView21
             // 
             webView21.AllowExternalDrop = true;
@@ -68,18 +56,16 @@
             webView21.DefaultBackgroundColor = Color.White;
             webView21.Location = new Point(0, 2);
             webView21.Name = "webView21";
-            webView21.Size = new Size(270, 754);
+            webView21.Size = new Size(270, 1000);
             webView21.TabIndex = 15;
             webView21.ZoomFactor = 1D;
             // 
             // MainContents
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
+            AutoScaleMode = AutoScaleMode.None;
             BackColor = Color.White;
-            ClientSize = new Size(700, 800);
+            ClientSize = new Size(700, 1000);
             Controls.Add(webView21);
-            Controls.Add(pictureBox7);
             FormBorderStyle = FormBorderStyle.None;
             MaximizeBox = false;
             MdiChildrenMinimizedAnchorBottom = false;
@@ -90,14 +76,12 @@
             SizeGripStyle = SizeGripStyle.Hide;
             Text = "Form2";
             Resize += OnResize;
-            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
             ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
         private ImageList imageList1;
-        private PictureBox pictureBox7;
         private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
         private System.Windows.Forms.Timer timer1;
     }
